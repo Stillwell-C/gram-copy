dc7fcd3cd9dad10bc7d77a924a3f9d876b04f88f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-01-12 18:47:15 +0900

    Calls ref on last card in feed

---
+++ src/components/ImageFeed/ImageFeed.js
@@ -0,0 +1,68 @@
+import { useState, useRef, useCallback, useEffect } from "react";
+import useGetPosts from "../../hooks/useGetPosts";
+import "./imageFeed.css";
+import ImgFeedCard from "../imageFeedCard/ImgFeedCard";
+import { getURL } from "../../firebase";
+
+const ImageFeed = () => {
+  const [pageNum, setPageNum] = useState(1);
+  const { loading, error, errorInfo, posts, hasMoreData } =
+    useGetPosts(pageNum);
+
+  const observer = useRef();
+  const lastPostRef = useCallback(
+    (post) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((posts) => {
+        if (posts[0].isIntersecting && hasMoreData) {
+          setPageNum((prev) => prev + 1);
+          console.log("near last post");
+        }
+      });
+
+      if (post) observer.current.observe(post);
+    },
+    [loading, hasMoreData]
+  );
+
+  useEffect(() => {
+    console.log("useEffect", posts);
+    console.log("more data", hasMoreData);
+  }, [posts]);
+
+  const content = posts.map((post, i) => {
+    if (posts.length === i + 1) {
+      return <ImgFeedCard key={post.id} post={post} ref={lastPostRef} />;
+    }
+    return <ImgFeedCard key={post.id} post={post} />;
+  });
+
+  return (
+    <div className='feedContainer'>
+      <>
+        {content}
+        {loading && (
+          <div className='lds-spinner'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+        {error && errorInfo.message}
+      </>
+    </div>
+  );
+};
+
+export default ImageFeed;
