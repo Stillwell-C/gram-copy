0b9ac486af19a14bcf6d21043ad823c9fc02b60e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-01-31 18:54:22 +0900

    Add exports to use in other files

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage, getDownloadURL, ref } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRymZXoBnlDpO5P_62Yth8--Gs5ANczNo",
@@ -13,3 +15,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth();
+export const getURL = async (imgLoc) => {
+  const url = await getDownloadURL(ref(storage, imgLoc));
+  return url;
+};
