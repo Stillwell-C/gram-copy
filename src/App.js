a0c736e69eb971ad65118562b331331d12404c36
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-01-31 18:56:08 +0900

    Test feed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import "./App.css";
-import { useState, useRef, useCallback } from "react";
-import useGetPosts from "./hooks/useGetPosts";
+import { Routes, Route } from "react-router-dom";
+import ImageFeed from "./pages/imageFeed/ImageFeed";
+import Login from "./pages/login/Login";
+import Navbar from "./components/navbar/Navbar";
+import UserFeed from "./pages/userFeed/UserFeed";
 
 function App() {
-  const [pageNum, setPageNum] = useState(0);
-  const { loading, error, errorInfo, posts, hasMoreData } =
-    useGetPosts(pageNum);
-
-  const observer = useRef();
-  const lastPostRef = useCallback(
-    (post) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver((posts) => {
-        if (posts[0].isIntersecting && hasMoreData) {
-          setPageNum((prev) => prev + 1);
-        }
-      });
-
-      if (post) observer.current.observe(post);
-    },
-    [loading, hasMoreData]
-  );
-
-  if (error) return <p>Error: {error.message}</p>;
-
-  const content = posts.map((post, i) => {
-    console.log(posts);
-    if (posts.length === i + 1) {
-      return (
-        <div ref={lastPostRef} key={post.id}>
-          {post.post}
-        </div>
-      );
-    }
-    return <div key={post.post}>{post.post}</div>;
-  });
-
   return (
-    <div className='App'>
-      <>
-        {content}
-        {loading && <p>Loading...</p>}
-        {/* {error && errorInfo.message} */}
-      </>
+    <div className='app-container'>
+      <Routes>
+        <Route path='/' element={<UserFeed />} />
+        <Route path='/accounts/:loginPath' element={<Login />} />
+      </Routes>
     </div>
   );
 }
