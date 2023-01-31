ed9247aecd89141cf93301069d55fcda4ff10548
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-01-31 18:53:28 +0900

    Create a user feed page

---
+++ src/pages/userFeed/UserFeed.js
@@ -0,0 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
+import ImageFeed from "../imageFeed/ImageFeed";
+
+const UserFeed = () => {
+  return (
+    <>
+      <Navbar />
+      <ImageFeed />
+      <div></div>
+    </>
+  );
+};
+
+export default UserFeed;
