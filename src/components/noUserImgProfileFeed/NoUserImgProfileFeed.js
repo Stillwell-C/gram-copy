8ca687b7f09fb887326e0a2b5c028f43443547a6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-03 16:51:02 +0900

    Create component

---
+++ src/components/noUserImgProfileFeed/NoUserImgProfileFeed.js
@@ -0,0 +1,31 @@
+import React from "react";
+import camera from "../../assets/camera-player-multimedia-svgrepo-com.svg";
+
+const NoUserImgProfileFeed = () => {
+  return (
+    <div className='feed-empty-container'>
+      <div className='share-photo-info'>
+        <button
+          type='button'
+          className='camera-icon-button'
+          aria-label='upload your image'
+        >
+          <img src={camera} alt='camera' />
+        </button>
+        <h1 className='header-text'>Share Photos</h1>
+        <div className='sub-text'>
+          When you share photos, they will appear on your profile
+        </div>
+        <button
+          type='button'
+          className='bottom-upload-button'
+          aria-label='upload your image'
+        >
+          Share your first photo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NoUserImgProfileFeed;
