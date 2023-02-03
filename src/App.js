4b74b14f04fc4cb709cccee12c6dead9ca88047a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-03 16:48:49 +0900

    Add route for profile

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ImageFeed from "./pages/imageFeed/ImageFeed";
 import Login from "./pages/login/Login";
 import Navbar from "./components/navbar/Navbar";
 import UserFeed from "./pages/userFeed/UserFeed";
+import ProfileMain from "./components/profileMain/ProfileMain";
 
 function App() {
   return (
     <div className='app-container'>
       <Routes>
         <Route path='/' element={<UserFeed />} />
+        <Route path='/:userParam' element={<ProfileMain />} />
         <Route path='/accounts/:loginPath' element={<Login />} />
       </Routes>
     </div>
