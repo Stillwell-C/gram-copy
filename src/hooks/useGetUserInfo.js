e08a2e8b9957ff3af7575a8bf72ff185a2fa6621
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-03 16:51:42 +0900

    Create hook to retrieve user data

---
+++ src/hooks/useGetUserInfo.js
@@ -0,0 +1,65 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, getURL } from "../firebase";
+
+const useGetUserInfo = (queryInput, queryParameter) => {
+  const [email, setEmail] = useState("");
+  const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
+  const [fullname, setFullName] = useState("");
+  const [likedPosts, setLikedPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
+  const [userBio, setUserBio] = useState("");
+  const [userImg, setUserImg] = useState("");
+  const [userImgURL, setUserImgURL] = useState("");
+  const [userPosts, setUserPosts] = useState([]);
+  const [username, setUsername] = useState("");
+  const [allData, setAllData] = useState({});
+
+  useEffect(() => {}, []);
+
+  const getUserInfo = async () => {
+    try {
+      const userQuery = await getDocs(
+        query(
+          collection(db, "userInfo"),
+          where(queryParameter, "==", queryInput)
+        )
+      );
+      const userInfo = userQuery.docs[0].data();
+      const userImgURL = await getURL(userInfo.userImg);
+      setAllData({ ...userInfo, userImgURL: userImgURL });
+      setEmail(userInfo.email);
+      setFollowers([...userInfo.followers]);
+      setFollowing([...userInfo.following]);
+      setFullName(userInfo.fullname);
+      setLikedPosts([...userInfo.likedPosts]);
+      setSavedPosts([...userInfo.savedPosts]);
+      setUserBio(userInfo.userBio);
+      setUserImg(userInfo.userImg);
+      setUserImgURL(userInfo.userImgURL);
+      setUserPosts(userInfo.userPosts);
+      setUsername(userInfo.username);
+    } catch (err) {
+      console.log(err.message);
+      console.log(err.code);
+    }
+  };
+
+  return {
+    email,
+    followers,
+    following,
+    fullname,
+    likedPosts,
+    savedPosts,
+    userBio,
+    userImg,
+    userImgURL,
+    userPosts,
+    username,
+    allData,
+  };
+};
+
+export default useGetUserInfo;
