81a57ff5f25dc61265a7356e65c3eb0c3783aaec
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-06 15:24:38 +0900

    Create post card component

---
+++ src/components/profilePostCard/ProfilePostCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import message from "../../assets/message-bubble-svgrepo-com.svg";
+import heart from "../../assets/heart-svgrepo-com.svg";
+import "./profilePostCard.scss";
+
+const ProfilePostCard = ({ post }) => {
+  return (
+    <div className='post-card-container'>
+      <img
+        className='post-img'
+        alt={post.altText ? post.altText : "user upload"}
+        src={post.imgURL}
+      />
+      <div className='hover-div'>
+        <div className='icon-div'>
+          <div className='icon-row'>
+            <img src={heart} alt='heart icon' />
+            <span>{post.likedUsers.length}</span>
+          </div>
+          <div className='icon-row'>
+            <img src={message} alt='message bubble icon' />
+            <span>{post.comments.length}</span>
+          </div>
+        </div>
+        <div className='background'></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePostCard;
