cd714c4a0408c2cfac1590c5f14e0184524fbf83
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-07 16:28:06 +0900

    Add links & accounts buttons for logged out users

--- src/components/feedRightInfo/FeedRightInfo.js
+++ src/components/feedRightInfo/FeedRightInfo.js
@@ -1,23 +1,48 @@
 import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import Footer from "../footer/Footer";
 import "./feedRightInfo.scss";
 
 const FeedRightInfo = () => {
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/");
+    dispatch({
+      type: "LOGOUT",
+    });
+  };
 
   return (
     <div className='right-info-container'>
-      <div className='top-user-info'>
-        <img src={currentUser.userImgURL} alt='user profile' />
-        <div className='user-name-div'>
-          <div className='user-name-top'>{currentUser.username}</div>
-          <div className='user-name-bottom'>{currentUser.fullname}</div>
+      {currentUser && (
+        <div className='top-user-info'>
+          <img src={currentUser.userImgURL} alt='user profile' />
+          <div className='user-name-div'>
+            <Link to={`/${currentUser.username}`}>
+              <div className='user-name-top'>{currentUser.username}</div>
+            </Link>
+            <div className='user-name-bottom'>{currentUser.fullname}</div>
+          </div>
+          <div className='button-div'>
+            <button aria-label='click to log out' onClick={() => handleLogout}>
+              Log out
+            </button>
+          </div>
         </div>
-        <div className='button-div'>
-          <button>Switch</button>
+      )}
+      {!currentUser && (
+        <div className='login-div'>
+          <Link to='/accounts/login' aria-label='click to log in'>
+            <button>Log In</button>
+          </Link>
+          <Link to='/accounts/emailsignup' aria-label='click to sign up'>
+            <button>Sign Up</button>
+          </Link>
         </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
