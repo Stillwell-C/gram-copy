7da55045567d249d36c3f65807496d4bc4aaed47
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-10 17:59:00 +0900

    Add two edit profile pages

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,19 +3,24 @@ import { useNavigate, useParams } from "react-router-dom";
 import LoginForm from "../../components/loginForm/LoginForm";
 import SignupForm from "../../components/signupForm/SignupForm";
 import { AuthContext } from "../../context/authContext";
+import EditProfile from "../editProfile/EditProfile";
 import "./login.scss";
 
+//TODO: Possibly rename component to accounts
 const Login = () => {
   const { accountsPath } = useParams();
 
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => console.log(currentUser), [currentUser]);
+  useEffect(() => console.log("path === ", accountsPath), []);
 
   return (
     <div className='login-container'>
       {accountsPath === "login" && <LoginForm />}
       {accountsPath === "emailsignup" && <SignupForm />}
+      {accountsPath === "edit" && <EditProfile />}
+      {accountsPath === "password" && <EditProfile />}
     </div>
   );
 };
