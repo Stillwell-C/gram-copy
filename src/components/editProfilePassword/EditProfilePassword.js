ae5a38b9b8c2180a2772f5a8f3a871d34bde8bda
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-10 20:33:14 +0900

    Add change password function

--- src/components/editProfilePassword/EditProfilePassword.js
+++ src/components/editProfilePassword/EditProfilePassword.js
@@ -1,13 +1,53 @@
-import React, { useState } from "react";
+import {
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+  updatePassword,
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { auth } from "../../firebase";
 import useGetLoggedInUserInfo from "../../hooks/useGetLoggedInUserInfo";
 import "./editProfilePassword.scss";
 
 export const EditProfilePassword = () => {
   const { userImgURL, username } = useGetLoggedInUserInfo();
 
+  const { currentUser } = useContext(AuthContext);
+
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [buttonActive, setButtonActive] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    if (oldPassword.length && newPassword.length && confirmPassword.length) {
+      setButtonActive(true);
+      return;
+    }
+    setButtonActive(false);
+  }, [oldPassword, newPassword, confirmPassword]);
+
+  const handleChangePassword = async (e) => {
+    //TODO: handle errors
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (newPassword !== confirmPassword) {
+      setErrorMsg("Passwords do not match");
+      return;
+    }
+    try {
+      const credential = EmailAuthProvider.credential(
+        currentUser.email,
+        oldPassword
+      );
+      await reauthenticateWithCredential(user, credential);
+      await updatePassword(user, newPassword);
+    } catch (err) {
+      console.log(err.message);
+      console.log(err.code);
+    }
+  };
 
   return (
     <div className='edit-profile-password-container'>
@@ -19,7 +59,7 @@ export const EditProfilePassword = () => {
           <div className='username'>{username}</div>
         </div>
       </div>
-      <form>
+      <form onSubmit={handleChangePassword}>
         <div className='form-row'>
           <div className='form-left'>
             <label htmlFor='old-password'>Old password</label>
@@ -68,7 +108,13 @@ export const EditProfilePassword = () => {
         <div className='button-div'>
           <div className='button-div-right password'>
             <div className='submit-button-div'>
-              <button type='submit' className='submit-button'>
+              <button
+                type='submit'
+                className={
+                  buttonActive ? "submit-button active" : "submit-button"
+                }
+                disabled={!buttonActive}
+              >
                 Change password
               </button>
             </div>
