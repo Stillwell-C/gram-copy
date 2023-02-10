20acba73d8f953f55936a3eb70ff0a535e306559
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-10 18:24:22 +0900

    Create page for edits

---
+++ src/pages/editProfile/EditProfile.js
@@ -0,0 +1,20 @@
+import React from "react";
+import EditProfileMain from "../../components/editProfileMain/EditProfileMain";
+import Navbar from "../../components/navbar/Navbar";
+import "./editProfile.scss";
+
+//TODO: maybe combine with profile page into a 1/3 2/3 split page
+const EditProfile = () => {
+  return (
+    <div className='edit-profile-page-container'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='edit-main'>
+        <EditProfileMain />
+      </div>
+    </div>
+  );
+};
+
+export default EditProfile;
