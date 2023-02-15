02fc468586b7ade263b2ef6ff7c9ba665223d136
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-15 10:19:57 +0900

    Change hooks used upon loading
    Fixed error of switching between user pages by getting functions
    from the getUserInfo & getLoggedInUserInfo hooks. These are now
    called in a useEffect that is triggered anytime the params change
    and then update an object in state. The page information is now based
    on this object.

--- src/components/profileMain/ProfileMain.js
+++ src/components/profileMain/ProfileMain.js
@@ -10,17 +10,18 @@ import sprocket from "../../assets/gear-wide-svgrepo-com.svg";
 import grid from "../../assets/grid-svgrepo-com.svg";
 import bookmark from "../../assets/bookmark-svgrepo-com.svg";
 import tagged from "../../assets/user-square-svgrepo-com.svg";
+import defaultProfilePic from "../../assets/Default_pfp.svg";
 import PostFeed from "../postFeed/PostFeed";
 import useUploadProfileImg from "../../hooks/useUploadProfileImg";
 import useGetLoggedInUserInfo from "../../hooks/useGetLoggedInUserInfo";
+import useGetUserInfoFunction from "../../hooks/useGetUserInfoFunction";
+import useGetLoggedInUserInfoFunction from "../../hooks/useGetLoggedInUserInfoFunction";
 
 const ProfileMain = () => {
   const { userParam } = useParams();
   const { currentUser } = useContext(AuthContext);
-  const { userPosts, followers, following, fullname, userBio, userImgURL } =
-    useGetUserInfo(userParam, "username");
-  const { likedPosts: userLikedPosts, savedPosts: userSavedPosts } =
-    useGetLoggedInUserInfo();
+  const getPageInfo = useGetUserInfoFunction();
+  const getUserInfo = useGetLoggedInUserInfoFunction();
 
   const uploadImg = useUploadProfileImg();
 
@@ -28,16 +29,42 @@ const ProfileMain = () => {
 
   const [pageLoading, setPageLoading] = useState(true);
   const [displaySelector, setDisplaySelector] = useState("posts");
-  const [updatedImgURL, setUpdatedImgURL] = useState("");
+  const [pageInfo, setPageInfo] = useState({});
 
   useEffect(() => {
     setPageLoading(true);
-  }, []);
+    setPageInfo({
+      pageUserPosts: [],
+      pageFollowers: [],
+      pageFollowing: [],
+      pageFullname: [],
+      pageUserBio: [],
+      pageUserImgURL: defaultProfilePic,
+      userLikedPosts: [],
+      userSavedPosts: [],
+    });
+    const getAllPageData = async () => {
+      const pageInfo = await getPageInfo(userParam, "username");
+      const userInfo = await getUserInfo();
+      const dataObj = {
+        pageUserPosts: pageInfo.userPosts,
+        pageFollowers: pageInfo.followers,
+        pageFollowing: pageInfo.following,
+        pageFullname: pageInfo.fullname,
+        pageUserBio: pageInfo.userBio,
+        pageUserImgURL: pageInfo.userImgURL,
+        userLikedPosts: userInfo.likedPosts,
+        userSavedPosts: userInfo.savedPosts,
+      };
+      setPageInfo(dataObj);
+      console.log("info", dataObj);
+    };
+    getAllPageData();
+  }, [userParam]);
 
   useEffect(() => {
-    setUpdatedImgURL(userImgURL);
     setPageLoading(false);
-  }, [userImgURL]);
+  }, [pageInfo.pageUserImgURL]);
 
   const handleImgClick = () => {
     imgInputRef.current.click();
@@ -46,7 +73,10 @@ const ProfileMain = () => {
   const handleImgUpload = (e) => {
     e.preventDefault();
     if (e.target.files && e.target.files[0]) {
-      setUpdatedImgURL(URL.createObjectURL(e.target.files[0]));
+      setPageInfo({
+        ...pageInfo,
+        pageUserImgURL: URL.createObjectURL(e.target.files[0]),
+      });
       uploadImg(e.target.files[0]);
     }
   };
@@ -62,7 +92,7 @@ const ProfileMain = () => {
                 aria-label='click to change profile photo'
                 onClick={handleImgClick}
               >
-                <img src={updatedImgURL} alt='user profile' />
+                <img src={pageInfo.pageUserImgURL} alt='user profile' />
               </button>
               <form>
                 <input
@@ -93,24 +123,32 @@ const ProfileMain = () => {
               </div>
               <div className='user-info-middle'>
                 <div>
-                  <span className='user-figure'>{userPosts.length}</span>
+                  <span className='user-figure'>
+                    {pageInfo.pageUserPosts.length}
+                  </span>
                   <span className='category'>
-                    {userPosts.length === 1 ? "post" : "posts"}
+                    {pageInfo.pageUserPosts.length === 1 ? "post" : "posts"}
                   </span>
                 </div>
                 <div>
-                  <span className='user-figure'>{followers.length}</span>
-                  {followers.length === 1 ? "follower" : "followers"}
+                  <span className='user-figure'>
+                    {pageInfo.pageFollowers.length}
+                  </span>
+                  {pageInfo.pageFollowers.length === 1
+                    ? "follower"
+                    : "followers"}
                   <span className='category'></span>
                 </div>
                 <div>
-                  <span className='user-figure'>{following.length}</span>
+                  <span className='user-figure'>
+                    {pageInfo.pageFollowing.length}
+                  </span>
                   following<span className='category'></span>
                 </div>
               </div>
               <div className='user-info-bottom'>
-                <div className='user-fullname'>{fullname}</div>
-                <div className='user-bio'>{userBio}</div>
+                <div className='user-fullname'>{pageInfo.pageFullname}</div>
+                <div className='user-bio'>{pageInfo.pageUserBio}</div>
               </div>
             </div>
           </div>
@@ -161,27 +199,27 @@ const ProfileMain = () => {
               {displaySelector === "posts" && (
                 <PostFeed
                   userParam={userParam}
-                  userPosts={userPosts}
-                  userLikedPosts={userLikedPosts}
-                  userSavedPosts={userSavedPosts}
+                  userPosts={pageInfo.pageUserPosts}
+                  userLikedPosts={pageInfo.userLikedPosts}
+                  userSavedPosts={pageInfo.userSavedPosts}
                   userQueryInput={"posts"}
                 />
               )}
               {displaySelector === "saved" && (
                 <PostFeed
                   userParam={userParam}
-                  userPosts={userPosts}
-                  userLikedPosts={userLikedPosts}
-                  userSavedPosts={userSavedPosts}
+                  userPosts={pageInfo.pageUserPosts}
+                  userLikedPosts={pageInfo.userLikedPosts}
+                  userSavedPosts={pageInfo.userSavedPosts}
                   userQueryInput={"saved"}
                 />
               )}
               {displaySelector === "tagged" && (
                 <PostFeed
                   userParam={userParam}
-                  userPosts={userPosts}
-                  userLikedPosts={userLikedPosts}
-                  userSavedPosts={userSavedPosts}
+                  userPosts={pageInfo.pageUserPosts}
+                  userLikedPosts={pageInfo.userLikedPosts}
+                  userSavedPosts={pageInfo.userSavedPosts}
                   userQueryInput={"tagged"}
                 />
               )}
