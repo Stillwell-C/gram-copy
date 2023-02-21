4b526650ebb525fba879debda1d6578cdaf5e426
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-21 17:35:38 +0900

    Change to use onAuthStateChanged

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,22 +1,47 @@
-import { createContext, useReducer, useEffect } from "react";
-import AuthReducer from "./authReducer";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useReducer, useEffect, useState } from "react";
+import { auth } from "../firebase";
 
-const INITIAL_STATE = {
-  currentUser: JSON.parse(localStorage.getItem("user")) || null,
-};
+// import AuthReducer from "./authReducer";
 
-export const AuthContext = createContext(INITIAL_STATE);
+// const INITIAL_STATE = {
+//   currentUser: JSON.parse(localStorage.getItem("user")) || null,
+// };
+
+// export const AuthContext = createContext(INITIAL_STATE);
+
+// export const AuthContextProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+//   //TODO: make an expiry method for local storage items
+//   useEffect(() => {
+//     localStorage.setItem("user", JSON.stringify(state.currentUser));
+//   }, [state.currentUser]);
+
+//   return (
+//     <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  const [currentUser, setCurrentUser] = useState({});
 
-  //TODO: make an expiry method for local storage items
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(state.currentUser));
-  }, [state.currentUser]);
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+    <AuthContext.Provider value={{ currentUser }}>
       {children}
     </AuthContext.Provider>
   );
