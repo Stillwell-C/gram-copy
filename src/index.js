9ba6ede9d23ec779ebf63b8cbc9a1bbf3f74eece
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-24 19:07:08 +0900

    Add chat context provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
 import { BrowserRouter } from "react-router-dom";
+import { ChatContextProvider } from "./context/chatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <ChatContextProvider>
+          <App />
+        </ChatContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
