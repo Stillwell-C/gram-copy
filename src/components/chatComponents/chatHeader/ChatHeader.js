e6909f29e639f4254587fb5719e6f57370e3cd15
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-25 18:39:30 +0900

    Create chatHeader

---
+++ src/components/chatComponents/chatHeader/ChatHeader.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ChatContext } from "../../../context/chatContext";
+
+import infoSvg from "../../../assets/info-circle-svgrepo-com.svg";
+import "./chatHeader.scss";
+
+const ChatHeader = () => {
+  const { userData } = useContext(ChatContext);
+  console.log(userData);
+
+  return (
+    <div className='chat-header-container'>
+      <div className='user-img-div'>
+        <Link
+          to={`/${userData.user.displayName}`}
+          aria-label="click to go to user's profile"
+        >
+          <img src={userData.user.photoURL} alt='user profile' />
+        </Link>
+      </div>
+      <div className='user-info-div'>
+        <Link
+          to={`/${userData.user.displayName}`}
+          aria-label="click to go to user's profile"
+        >
+          <span className='username-text'>{userData.user.displayName}</span>
+        </Link>
+      </div>
+      <div className='button-div'>
+        <button>
+          <img src={infoSvg} alt='information icon' />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
