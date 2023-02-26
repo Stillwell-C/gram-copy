c81d9d7564f4c3d71e0408aba23cbad8e93ad5ec
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-02-26 14:55:24 +0900

    Add function to determine time display

--- src/components/chatComponents/chatBody/ChatBody.js
+++ src/components/chatComponents/chatBody/ChatBody.js
@@ -1,5 +1,6 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useRef, useState } from "react";
+import { AuthContext } from "../../../context/authContext";
 import { ChatContext } from "../../../context/chatContext";
 import { db } from "../../../firebase";
 import ChatMessage from "../chatMessage/ChatMessage";
@@ -7,7 +8,9 @@ import "./chatBody.scss";
 
 const ChatBody = () => {
   const [messages, setMessages] = useState([]);
+  const [chatMessages, setChatMessages] = useState([]);
 
+  const { currentUser } = useContext(AuthContext);
   const { userData } = useContext(ChatContext);
 
   useEffect(() => {
@@ -18,13 +21,34 @@ const ChatBody = () => {
     return () => unsub();
   }, [userData.chatId]);
 
-  return (
-    <div className='chat-body-container'>
-      {messages.map((message) => (
-        <ChatMessage message={message} key={message.id} />
-      ))}
-    </div>
-  );
+  useEffect(() => {
+    const messageArr = [];
+    let lastMsg = null;
+
+    for (let message of messages) {
+      let time = false;
+      if (!lastMsg) {
+        time = true;
+      }
+      if (
+        lastMsg &&
+        Math.abs(message.date.toDate() - lastMsg.date.toDate()) >= 350000
+      ) {
+        time = true;
+      }
+      if (lastMsg && lastMsg.senderId !== message.senderId) {
+        time = true;
+      }
+      lastMsg = message;
+      messageArr.push(
+        <ChatMessage message={message} key={message.id} time={time} />
+      );
+    }
+
+    setChatMessages(messageArr);
+  }, [messages]);
+
+  return <div className='chat-body-container'>{chatMessages}</div>;
 };
 
 export default ChatBody;
