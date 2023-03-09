66250ae7ae4875934a2f20eb5dd2d463e06448bf
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-03-09 17:25:56 +0900

    Add headerbar

--- src/pages/chats/Chats.js
+++ src/pages/chats/Chats.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatMain from "../../components/chatComponents/chatMain/ChatMain";
+import HeaderBar from "../../components/headerBar/HeaderBar";
 import Navbar from "../../components/navbar/Navbar";
 import "./chats.scss";
 
@@ -7,6 +8,7 @@ const Chats = () => {
   return (
     <div className='chats-page-container'>
       <div className='navbar'>
+        <HeaderBar />
         <Navbar />
       </div>
       <div className='chats-main'>
