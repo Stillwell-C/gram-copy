34351f91bfcb69a03138d4f2c9a5ee9787dd848d
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-27 17:17:03 +0900

    Initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./navbar/Navbar";
+import styles from "../scss/layout.module.scss";
+import "../scss/global.scss";
+
+const Layout = () => {
+  //Use this to not display navbar on pages
+  const { pathname } = useLocation();
+
+  return (
+    <div className={styles.layout}>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
