2823fa0d415e50c69259c563e2b05ead6c56807f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-27 16:20:09 +0900

    Add redux store & change main route

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChatContextProvider } from "./context/chatContext";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <ChatContextProvider>
-          <App />
-        </ChatContextProvider>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <ChatContextProvider>
+            <Routes>
+              <Route path='/*' element={<App />} />
+            </Routes>
+          </ChatContextProvider>
+        </AuthContextProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
