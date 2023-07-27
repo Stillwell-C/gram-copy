16d7d8b4be66c25149841ffe48fd270a05de8aef
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-27 17:18:50 +0900

    Remove navbar

--- src/pages/userFeed/UserFeed.js
+++ src/pages/userFeed/UserFeed.js
@@ -1,20 +1,13 @@
 import FeedRightInfo from "../../components/feedRightInfo/FeedRightInfo";
-import HeaderBar from "../../components/headerBar/HeaderBar";
-import Navbar from "../../components/navbar/Navbar";
 import ImageFeed from "../imageFeed/ImageFeed";
 import "./userfeed.scss";
 
 const UserFeed = () => {
   return (
-    <div className='userfeed-container'>
-      <div>
-        <Navbar />
-      </div>
-      <section className='userfeed-main'>
-        <ImageFeed />
-        <FeedRightInfo />
-      </section>
-    </div>
+    <main className='userfeed-main'>
+      <ImageFeed />
+      <FeedRightInfo />
+    </main>
   );
 };
 
