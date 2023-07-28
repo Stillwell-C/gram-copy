492a2313a85a9a500651827283191cd02d5755df
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-28 11:24:16 +0900

    Remove Nav from login/signup pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,9 +7,18 @@ const Layout = () => {
   //Use this to not display navbar on pages
   const { pathname } = useLocation();
 
+  let displayNav = true;
+  if (
+    pathname.match(/\/accounts\/login/i) ||
+    pathname.match(/\/accounts\/emailsignup/i)
+  ) {
+    displayNav = false;
+  }
+  console.log("pathname: ", pathname);
+
   return (
     <div className={styles.layout}>
-      <Navbar />
+      {displayNav && <Navbar />}
       <div className={styles.outlet}>
         <Outlet />
       </div>
