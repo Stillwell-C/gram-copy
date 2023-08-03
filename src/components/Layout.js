e8955b72b9e3f79a6c77ef77d02a55948f0a726f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-03 15:26:07 +0900

    Remove sidebar display when credentialsLoading

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,21 @@ import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
 import styles from "../scss/layout.module.scss";
 import "../scss/global.scss";
+import { useSelector } from "react-redux";
+import { selectCredentialsLoading } from "../features/auth/authSlice";
 
 const Layout = () => {
   //Use this to not display navbar on pages
   const { pathname } = useLocation();
+  const authLoading = useSelector(selectCredentialsLoading);
 
   let displayNav = true;
   if (
     pathname.match(/\/accounts\/login/i) ||
     pathname.match(/\/accounts\/emailsignup/i)
   ) {
+    displayNav = false;
+  } else if (authLoading) {
     displayNav = false;
   }
 
