74bb350c0e918eeba8f1068a46ac125a8644104b
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-03 16:04:22 +0900

    Initial commit

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,21 @@
+import jwtDecode from "jwt-decode";
+import { useSelector } from "react-redux";
+import { selectCurrentToken } from "../features/auth/authSlice";
+
+const useAuth = () => {
+  const token = useSelector(selectCurrentToken);
+  let isAdmin = false;
+
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    console.log(decodedToken);
+    const { username, id, img, roles } = decodedToken.UserInfo;
+    isAdmin = roles?.some((role) => role.match(/admin/i));
+
+    return { username, id, img, isAdmin, loggedIn: true };
+  }
+
+  return { username: "", id: "", img: "", isAdmin, loggedIn: false };
+};
+
+export default useAuth;
