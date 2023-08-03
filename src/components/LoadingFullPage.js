30eeace67d2db3518e5e202b1249c1d39e42ce02
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-03 15:42:54 +0900

    Update styling

--- src/components/LoadingFullPage.js
+++ src/components/LoadingFullPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import gramImg from "../assets/instagram-svgrepo-com.svg";
 import Styles from "../scss/globalStyles.module.scss";
+import LayoutStyles from "../scss/layout.module.scss";
 import "../scss/loadingFullPage.scss";
 
 const LoadingFullPage = () => {
   return (
     <div
-      className={`${Styles.flexGrow1} ${Styles.flexContainer} ${Styles.flexAlignCenter} ${Styles.flexJustifyCenter}`}
+      className={`${LayoutStyles.layout} ${Styles.flexAlignCenter} ${Styles.flexJustifyCenter} loadingFullPage`}
     >
       <img src={gramImg} alt='Gram Copy Logo' />
     </div>
