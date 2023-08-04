e7fb4d093477f89217b457bc088bd91767f3d77e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-04 14:39:21 +0900

    Remove query & make display posts from props

--- src/components/postFeed/PostFeed.js
+++ src/components/postFeed/PostFeed.js
@@ -1,97 +1,24 @@
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { AuthContext } from "../../context/authContext";
-import useGetPostsCompound from "../../hooks/useGetPostsCompound";
-import useGetUserPosts from "../../hooks/useGetUserPosts";
+import React, { useState } from "react";
 import CreatePostModal from "../createPostModal/CreatePostModal";
 import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import NoUserImgProfileFeed from "../noUserImgProfileFeed/NoUserImgProfileFeed";
 import ProfilePostCard from "../profilePostCard/ProfilePostCard";
 
-const PostFeed = ({
-  userParam,
-  userPosts,
-  userQueryInput,
-  userLikedPosts,
-  userSavedPosts,
-}) => {
-  const [displayNoImgFeed, setDisplayNoImgFeed] = useState(false);
+const PostFeed = ({ posts, lastPostRef, isFetching, isError, error }) => {
   const [displayPostModal, setDisplayPostModal] = useState(false);
-  const [pageNum, setPageNum] = useState(1);
-  // const { loading, error, errorInfo, posts, hasMoreData } = useGetUserPosts(
-  //   userParam,
-  //   pageNum,
-  //   userQueryInput
-  // );
-  const { loading, error, errorInfo, posts, hasMoreData } = useGetPostsCompound(
-    pageNum,
-    userParam,
-    "==",
-    userQueryInput,
-    9,
-    6
-  );
-
-  const { currentUser } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (
-      !loading &&
-      userPosts.length < 1 &&
-      userQueryInput === "posts" &&
-      currentUser?.displayName === userParam
-    ) {
-      setDisplayNoImgFeed(true);
-      return;
-    }
-    setDisplayNoImgFeed(false);
-  }, [userPosts, loading]);
-
-  const observer = useRef();
-  const lastPostRef = useCallback(
-    (post) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver((posts) => {
-        if (posts[0].isIntersecting && hasMoreData) {
-          setPageNum((prev) => prev + 1);
-          console.log("near last post");
-        }
-      });
-
-      if (post) observer.current.observe(post);
-    },
-    [loading, hasMoreData]
-  );
 
   const content = posts.map((post, i) => {
     if (posts.length === i + 1) {
       return (
         <ProfilePostCard
-          key={post.id}
+          key={post._id}
           post={post}
           postType={"post"}
-          userLikedPosts={userLikedPosts}
-          userSavedPosts={userSavedPosts}
           ref={lastPostRef}
         />
       );
     }
-    return (
-      <ProfilePostCard
-        key={post.id}
-        post={post}
-        postType={"post"}
-        userLikedPosts={userLikedPosts}
-        userSavedPosts={userSavedPosts}
-      />
-    );
+    return <ProfilePostCard key={post._id} post={post} postType={"post"} />;
   });
 
   const handleAddPostModal = () => {
@@ -102,11 +29,11 @@ const PostFeed = ({
     <div className='user-posts-container'>
       <>
         {content}
-        {loading && <LoadingSpinner />}
-        {!loading && displayNoImgFeed && (
+        {isFetching && <LoadingSpinner />}
+        {posts.length <= 0 && (
           <NoUserImgProfileFeed handleAddPostModal={handleAddPostModal} />
         )}
-        {error && errorInfo.message}
+        {isError && error?.data?.message}
         {displayPostModal && (
           <CreatePostModal setDisplayPostModal={setDisplayPostModal} />
         )}
