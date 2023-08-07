b787a316dbcff9ee9d0c345345677bc29c5d9a7d
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-07 18:14:54 +0900

    Initial commit

---
+++ src/components/AddCommentForm.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useAddNewCommentMutation } from "../features/comments/commentsApiSlice";
+
+const AddCommentForm = ({ post }) => {
+  const { authenticatedUser, id } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [comment, setComment] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const [addComment, { isLoading, isError, error, isSuccess }] =
+    useAddNewCommentMutation();
+
+  useEffect(() => {
+    if (isError) console.log(error);
+  }, [isError]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setComment("");
+    }
+  }, [isSuccess]);
+
+  const handleAddComment = (e) => {
+    e.preventDefault();
+    if (!authenticatedUser) {
+      navigate("/accounts/login");
+      return;
+    }
+    addComment({ author: id, parentPostId: post._id, commentBody: comment });
+  };
+
+  useEffect(() => {
+    if (isLoading || comment.length < 1) {
+      setButtonDisabled(true);
+      return;
+    }
+    setButtonDisabled(false);
+  }, [comment.length, isLoading]);
+
+  return (
+    <div className='input-comment-div'>
+      <form onSubmit={handleAddComment}>
+        <div className='input-left'>
+          <label>
+            <input
+              type='text'
+              maxLength={2200}
+              placeholder='Add a comment...'
+              onChange={(e) => setComment(e.target.value)}
+              value={comment}
+              disabled={isLoading}
+            />
+          </label>
+        </div>
+        <div className='input-right'>
+          <button
+            type='submit'
+            className={`${buttonDisabled ? "disabled" : ""}`}
+            disabled={buttonDisabled}
+            aria-disabled={buttonDisabled ? "true" : "false"}
+          >
+            Post
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddCommentForm;
