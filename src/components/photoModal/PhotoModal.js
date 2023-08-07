e8f1319fd91ef12e75d46db03bb92ff0750dff29
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-07 17:24:53 +0900

    Move comment rendering to separate component

--- src/components/photoModal/PhotoModal.js
+++ src/components/photoModal/PhotoModal.js
@@ -1,40 +1,24 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import threeDots from "../../assets/three-dots-line-svgrepo-com.svg";
-import addCommentIcon from "../../assets/add-circle-svgrepo-com.svg";
 import commentBubble from "../../assets/message-circle-01-svgrepo-com.svg";
 import message from "../../assets/plane-svgrepo-com.svg";
 import filledBookmark from "../../assets/bookmark-filled.svg";
 import outlinedBookmark from "../../assets/bookmark-outline.svg";
 import outlinedHeart from "../../assets/heart-rounded-svgrepo-com.svg";
 import filledHeart from "../../assets/heart-svgrepo-com.svg";
-import Comment from "../comment/Comment";
 import moment from "moment";
-import { AuthContext } from "../../context/authContext";
 import "./photoModal.scss";
-import useAddComment from "../../hooks/useAddComment";
 import useFollowUnfollow from "../../hooks/useFollowUnfollow";
-import useGetLoggedInUserInfoFunction from "../../hooks/useGetLoggedInUserInfoFunction";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
 import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import AdditionalOptionsModal from "../additionalOptionsModal/AdditionalOptionsModal";
 import ReportModal from "../reportModal/ReportModal";
+import useAuth from "../../hooks/useAuth";
+import PhotoModalComments from "../PhotoModalComments";
 
-const PhotoModal = ({
-  setShowPhotoModal,
-  userImgURL,
-  post,
-  liked,
-  saved,
-  handleLike,
-  handleSave,
-  likesOffset,
-}) => {
-  const { currentUser } = useContext(AuthContext);
-  const [commentsPool, setCommentsPool] = useState([]);
-  const [commentsArr, setCommentsArr] = useState([]);
-  const [commentsLoading, setCommentsLoading] = useState(true);
+const PhotoModal = ({ setShowPhotoModal, post, handleLike, handleSave }) => {
+  const { authenticatedUser, id } = useAuth();
+
   const [picInfoButton, setPicInfoButton] = useState();
   const [activateButton, setActivateButton] = useState(false);
   const [comment, setComment] = useState("");
@@ -43,37 +27,12 @@ const PhotoModal = ({
     useState(false);
   const [showReportModal, setShowReportModal] = useState(false);
 
-  const getUserInfo = useGetLoggedInUserInfoFunction();
-  const addComment = useAddComment();
   const navigate = useNavigate();
   const commentRef = useRef(null);
   const { follow: followUser, unfollow: unfollowUser } = useFollowUnfollow();
 
-  useEffect(() => {
-    const fetchComments = async () => {
-      const fetchedPost = await getDoc(doc(db, "userImgs", post.id));
-      const postInfo = fetchedPost.data();
-      setCommentsPool([...postInfo.comments]);
-    };
-    const determineFriend = async () => {
-      const fetchedUserInfo = await getUserInfo();
-      const friendArr = fetchedUserInfo.following.filter(
-        (user) => user === post.userUid
-      );
-      // const friendStatus = fetchedUserInfo.following.includes(post.userID);
-      setIsFriend(friendArr.length ? true : false);
-    };
-    fetchComments();
-    currentUser?.displayName && determineFriend();
-  }, []);
-
-  useEffect(() => {
-    loadComments();
-  }, [commentsPool]);
-
-  useEffect(() => {
-    setCommentsLoading(false);
-  }, [commentsArr]);
+  const userImgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_150,c_fill/${post?.user?.userImgKey}`;
+  const imgURL = `https://res.cloudinary.com/danscxcd2/image/upload/${post?.imgKey}`;
 
   useEffect(() => {
     if (comment.length) {
@@ -82,30 +41,6 @@ const PhotoModal = ({
     }
     setActivateButton(false);
   }, [comment]);
-
-  const loadComments = () => {
-    const start = commentsArr.length;
-    const end =
-      start + 10 > commentsPool.length ? commentsPool.length : start + 10;
-    const newArr = [];
-
-    for (let i = start; i < end; i++) {
-      newArr.push(commentsPool[i]);
-    }
-
-    setCommentsArr([...commentsArr, ...newArr]);
-  };
-
-  const comments = commentsArr.map((comment) => (
-    <Comment
-      comment={comment}
-      abbreviate={false}
-      showTime={true}
-      showImage={true}
-      setShowPhotoModal={setShowPhotoModal}
-      key={comment.date + comment.userName}
-    />
-  ));
 
   useEffect(() => {
     if (isFriend) {
@@ -131,18 +66,8 @@ const PhotoModal = ({
     );
   }, [isFriend]);
 
-  const handleAddComment = (e) => {
-    e.preventDefault();
-    if (!currentUser) {
-      navigate("/accounts/login");
-      return;
-    }
-    addComment(currentUser.displayName, comment, post);
-    setComment("");
-  };
-
   const handleFollow = () => {
-    if (!currentUser) {
+    if (!authenticatedUser) {
       navigate("/accounts/login");
       return;
     }
@@ -151,7 +76,7 @@ const PhotoModal = ({
   };
 
   const handleUnfollow = () => {
-    if (!currentUser) {
+    if (!authenticatedUser) {
       navigate("/accounts/login");
       return;
     }
@@ -175,12 +100,12 @@ const PhotoModal = ({
               <div className='photo-info-div'>
                 <div className='photo-info-top'>
                   <Link
-                    to={`/${post.userName}`}
-                    aria-label={`move to ${post.userName}'s profile`}
+                    to={`/${post.user.username}`}
+                    aria-label={`move to ${post.user.username}'s profile`}
                   >
-                    <div className='userName'>{post.userName}</div>
+                    <div className='userName'>{post.user.username}</div>
                   </Link>
-                  {currentUser?.displayName !== post.userName && (
+                  {id !== post.user._id && (
                     <>
                       <span>•</span>
                       {picInfoButton}
@@ -205,7 +130,7 @@ const PhotoModal = ({
           <div className='photo-modal-left'>
             <div className='img-container'>
               <img
-                src={post.imgURL}
+                src={imgURL}
                 alt={post.altText ? post.altText : "user upload"}
               />
             </div>
@@ -218,12 +143,12 @@ const PhotoModal = ({
               <div className='photo-info-div'>
                 <div className='photo-info-top'>
                   <Link
-                    to={`/${post.userName}`}
-                    aria-label={`move to ${post.userName}'s profile`}
+                    to={`/${post.user.username}`}
+                    aria-label={`move to ${post.user.username}'s profile`}
                   >
-                    <div className='userName'>{post.userName}</div>
+                    <div className='userName'>{post.user.username}</div>
                   </Link>
-                  {currentUser?.displayName !== post.userName && (
+                  {id !== post.user._id && (
                     <>
                       <span>•</span>
                       {picInfoButton}
@@ -242,32 +167,24 @@ const PhotoModal = ({
               </button>
             </div>
             <div className='photo-modal-right-middle'>
-              {!commentsLoading && comments}
-              {commentsLoading && <LoadingSpinner />}
-              {commentsArr.length < commentsPool.length && (
-                <div className='add-comments-button-div'>
-                  <button
-                    type='button'
-                    aria-label='click to show more comments'
-                    title='click to show more comments'
-                    onClick={loadComments}
-                  >
-                    <img src={addCommentIcon} alt='' aria-hidden='true' />
-                  </button>
-                </div>
-              )}
+              <PhotoModalComments
+                post={post}
+                setShowPhotoModal={setShowPhotoModal}
+              />
             </div>
             <div className='photo-modal-right-bottom'>
               <div className='buttons'>
                 <div className='buttons-left'>
                   <button
                     className='likeButton'
-                    aria-label={`click to ${liked ? "unlike" : "like"} post`}
+                    aria-label={`click to ${
+                      post.isLiked ? "unlike" : "like"
+                    } post`}
                     onClick={handleLike}
                   >
                     <img
-                      src={liked ? filledHeart : outlinedHeart}
-                      className={liked ? "filled heart" : "heart"}
+                      src={post.isLiked ? filledHeart : outlinedHeart}
+                      className={post.isLiked ? "filled heart" : "heart"}
                       alt=''
                       aria-hidden='true'
                     />
@@ -286,27 +203,31 @@ const PhotoModal = ({
                 <div className='buttons-right'>
                   <button
                     className='bookmarkButton'
-                    aria-label={`click to ${saved ? "save" : "unsave"} post`}
+                    aria-label={`click to ${
+                      post.isSave ? "save" : "unsave"
+                    } post`}
                     onClick={handleSave}
                   >
                     <img
-                      src={saved ? filledBookmark : outlinedBookmark}
+                      src={post.isSaved ? filledBookmark : outlinedBookmark}
                       alt=''
                       aria-hidden='true'
-                      className={saved ? "filled" : ""}
+                      className={post.isSaved ? "filled" : ""}
                     />
                   </button>
                 </div>
               </div>
               <div className='photo-bottom-text'>
                 <div className='likes-counter'>
-                  {post.likedUsers.length + likesOffset}{" "}
+                  {/* {post.likedUsers.length + likesOffset}{" "}
                   {post.likedUsers.length + likesOffset === 1
                     ? "Like"
-                    : "Likes"}
+                    : "Likes"} */}
+                  {post?.likes}
+                  {post?.likes === 1 ? " Like" : " Likes"}
                 </div>
                 <div className='time-ago'>
-                  {moment(post.date.toDate()).fromNow().toUpperCase()}
+                  {moment(post.updatedAt).fromNow().toUpperCase()}
                 </div>
               </div>
               <div className='input-comment-div'>
@@ -329,7 +250,7 @@ const PhotoModal = ({
                       aria-label='click to submit comment'
                       className={activateButton ? "active" : "deactivated"}
                       disabled={!activateButton}
-                      onClick={handleAddComment}
+                      // onClick={handleAddComment}
                       aria-disabled={!activateButton}
                     >
                       Post
