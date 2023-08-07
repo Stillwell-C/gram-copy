175cb22cf6bcbec78e493943820d6bbfe6d97e72
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-07 11:57:14 +0900

    Initial commit

---
+++ src/features/likes/likesApiSlice.js
@@ -0,0 +1,61 @@
+import { apiSlice } from "../../app/api/apiSlice";
+
+export const likesApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getLikedUsers: builder.query({
+      query: ({ page, limit, postID }) => {
+        return {
+          url: `/postlike/${postID}?page=${page}&limit=${limit}`,
+          validateStatus: (response, result) => {
+            return response.status === 200 && !result.isError;
+          },
+        };
+      },
+      providesTags: (result, error, arg) => {
+        if (result) return [{ type: "PostLike", id: result._id }];
+        else return ["PostLike"];
+      },
+    }),
+    getLikedPosts: builder.query({
+      query: ({ page, limit, postID }) => {
+        return {
+          url: `/postlike/user/${postID}?page=${page}&limit=${limit}`,
+          validateStatus: (response, result) => {
+            return response.status === 200 && !result.isError;
+          },
+        };
+      },
+      providesTags: (result, error, arg) => {
+        if (result) return [{ type: "PostLike", id: result._id }];
+        else return ["PostLike"];
+      },
+    }),
+    addNewLike: builder.mutation({
+      query: ({ parentPostID, userID }) => ({
+        url: `/postlike/${parentPostID}`,
+        method: "POST",
+        body: { userID },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "PostLike", id: arg._id },
+      ],
+    }),
+    deleteLike: builder.mutation({
+      query: ({ parentPostID, userID }) => ({
+        url: `/postlike/${parentPostID}`,
+        method: "DELETE",
+        body: { userID },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "PostLike", id: arg._id },
+      ],
+    }),
+  }),
+});
+
+export const {
+  useGetLikedPostsQuery,
+  useGetLikedUsersQuery,
+  useAddNewLikeMutation,
+  useDeleteLikeMutation,
+} = likesApiSlice;
