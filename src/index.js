d107322b6b765125475adcbcc00a1cd98fbdc476
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-09 18:19:39 +0900

    Add react query provider

--- src/index.js
+++ src/index.js
@@ -6,20 +6,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChatContextProvider } from "./context/chatContext";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <ChatContextProvider>
-            <Routes>
-              <Route path='/*' element={<App />} />
-            </Routes>
-          </ChatContextProvider>
-        </AuthContextProvider>
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AuthContextProvider>
+            <ChatContextProvider>
+              <Routes>
+                <Route path='/*' element={<App />} />
+              </Routes>
+              <ReactQueryDevtools />
+            </ChatContextProvider>
+          </AuthContextProvider>
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
