f497d10fcee802710477c4bf28baf06aec242ccd
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-10 12:17:37 +0900

    remove console logs

--- src/features/posts/postApiRoutes.js
+++ src/features/posts/postApiRoutes.js
@@ -1,19 +1,18 @@
 import gramCopyApi from "../../app/api/gramCopyApi";
 
 export const getMultiplePosts = async ({ pageParam, ...args }) => {
-  console.log(pageParam);
-  console.log("args", args);
-
   const response = await gramCopyApi.get(`/posts`, {
     params: { page: pageParam, ...args },
   });
-  console.log(response);
   const postDataWithPage = response.data.posts.map((post) => ({
     ...post,
     pageNo: pageParam - 1,
   }));
-  console.log(postDataWithPage);
-  console.log(response.data);
+  console.log({
+    posts: postDataWithPage,
+    totalPosts: response.data.totalPosts,
+    page: pageParam,
+  });
   return {
     posts: postDataWithPage,
     totalPosts: response.data.totalPosts,
