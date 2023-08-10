7f98eb970075460b91ddb4a3820ffb66df0e07a6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-10 16:15:26 +0900

    Convert to use react query

--- src/components/PhotoModalComments.js
+++ src/components/PhotoModalComments.js
@@ -1,71 +1,64 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import addCommentIcon from "../assets/add-circle-svgrepo-com.svg";
-
-import { useGetPostCommentsQuery } from "../features/comments/commentsApiSlice";
 import Comment from "./comment/Comment";
 import LoadingSpinner from "./loadingSpinner/LoadingSpinner";
+import { useInfiniteQuery } from "react-query";
+import { getPostComments } from "../features/comments/commentsApiRoutes";
 
 const PhotoModalComments = ({ post, setShowPhotoModal }) => {
-  const [pageNum, setPageNum] = useState(1);
-  const [commentDataArr, setCommentDataArr] = useState([]);
-  const [hasMoreComments, setHasMoreComments] = useState(false);
-
-  useEffect(() => {
-    if (post.caption.length) {
-      const caption = {
-        commentBody: post.caption,
-        updatedAt: post.updatedAt,
-        author: {
-          username: post.user.username,
-          userImgKey: post.user.userImgKey,
-        },
-        _id: `${post._id}postCaption`,
-      };
-      setCommentDataArr([caption]);
-    }
-  }, []);
-
   const commentLoadLimit = 10;
 
   const {
     data: commentData,
     isLoading,
-    isFetching,
     isError,
     error,
-  } = useGetPostCommentsQuery({
-    postID: post._id,
-    page: pageNum,
-    limit: commentLoadLimit,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["comments", post._id],
+    queryFn: ({ pageParam = 1 }) =>
+      getPostComments({
+        pageParam,
+        limit: commentLoadLimit,
+        postID: post._id,
+      }),
+    refetchOnWindowFocus: false,
+    getNextPageParam: (lastPage, pages) => {
+      const pageLimit = Math.ceil(lastPage.totalComments / commentLoadLimit);
+      if (lastPage.page < pageLimit) return lastPage.page + 1;
+      return false;
+    },
   });
 
   useEffect(() => {
     if (isError) console.log(error);
   }, [isError]);
 
-  useEffect(() => {
-    if (isLoading || isFetching || !commentData) return;
-    setHasMoreComments(
-      (Math.ceil(commentData?.totalComments / commentLoadLimit) || 1) > pageNum
-    );
-    if (
-      commentDataArr.length &&
-      commentDataArr.filter(({ _id }) => _id === commentData?.comments[0]?._id)
-        .length > 0
-    ) {
-      const filteredCommentData = commentData?.comments?.filter(
-        (commentData) =>
-          !commentDataArr.some(
-            (commentDataArr) => commentData._id === commentDataArr._id
-          )
-      );
-      setCommentDataArr((prev) => [...prev, ...filteredCommentData]);
-      return;
-    }
-    setCommentDataArr((prev) => [...prev, ...commentData?.comments]);
-  }, [commentData]);
+  const flattenedFeedData = commentData?.pages?.reduce((acc, page) => {
+    return [...acc, ...page.comments];
+  }, []);
 
-  const comments = commentDataArr?.map((comment) => (
+  const captionComment = {
+    commentBody: post.caption,
+    updatedAt: post.updatedAt,
+    author: {
+      username: post.user.username,
+      userImgKey: post.user.userImgKey,
+    },
+    _id: `${post._id}postCaption`,
+  };
+
+  const commentsArr = post?.caption?.length
+    ? flattenedFeedData?.length
+      ? [captionComment, ...flattenedFeedData]
+      : [captionComment]
+    : flattenedFeedData?.length
+    ? [...flattenedFeedData]
+    : [];
+
+  const comments = commentsArr?.map((comment) => (
     <Comment
       comment={comment}
       abbreviate={false}
@@ -80,13 +73,13 @@ const PhotoModalComments = ({ post, setShowPhotoModal }) => {
     <>
       {comments}
       {(isLoading || isFetching) && <LoadingSpinner />}
-      {hasMoreComments && (
+      {hasNextPage && (
         <div className='add-comments-button-div'>
           <button
             type='button'
             aria-label='click to show more comments'
             title='click to show more comments'
-            onClick={() => setPageNum((prev) => prev + 1)}
+            onClick={fetchNextPage}
           >
             <img src={addCommentIcon} alt='' aria-hidden='true' />
           </button>
