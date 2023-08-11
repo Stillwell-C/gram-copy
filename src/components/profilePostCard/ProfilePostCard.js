607e05922c2d94a98ab97c23d0b0cfc9731e7b6b
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-11 16:11:37 +0900

    Send queryKey to PhotoModal

--- src/components/profilePostCard/ProfilePostCard.js
+++ src/components/profilePostCard/ProfilePostCard.js
@@ -9,96 +9,102 @@ import TagUsersModal from "../tagUsersModal/TagUsersModal";
 import useAuth from "../../hooks/useAuth";
 import DeletePostConfirmationModal from "../DeletePostConfirmationModal";
 
-const ProfilePostCard = React.forwardRef(({ post, profilePosts }, ref) => {
-  const { id } = useAuth();
+const ProfilePostCard = React.forwardRef(
+  ({ post, profilePosts, queryKey }, ref) => {
+    const { id } = useAuth();
 
-  const imgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_300,c_fill/${post?.imgKey}`;
+    const imgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_300,c_fill/${post?.imgKey}`;
 
-  const [showPhotoModal, setShowPhotoModal] = useState(false);
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
-  const [showTagUsersModal, setShowTagUsersModal] = useState(false);
+    const [showPhotoModal, setShowPhotoModal] = useState(false);
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+    const [showTagUsersModal, setShowTagUsersModal] = useState(false);
 
-  const cardContent = (
-    <>
-      <img
-        className='post-img'
-        alt={post.altText ? post.altText : "user upload"}
-        src={imgURL}
-      />
-      <div className='hover-div'>
-        {id === post.user._id && profilePosts && (
-          <div className='delete-btn-div'>
-            <button
-              className='delete-btn'
-              aria-label='click to delete this image'
-              onClick={() => setShowDeleteConfirmation(true)}
-            >
-              <img src={trashIcon} alt='trash can icon' />
-            </button>
+    const cardContent = (
+      <>
+        <img
+          className='post-img'
+          alt={post.altText ? post.altText : "user upload"}
+          src={imgURL}
+        />
+        <div className='hover-div'>
+          {id === post.user._id && profilePosts && (
+            <div className='delete-btn-div'>
+              <button
+                className='delete-btn'
+                aria-label='click to delete this image'
+                onClick={() => setShowDeleteConfirmation(true)}
+              >
+                <img src={trashIcon} alt='trash can icon' />
+              </button>
+            </div>
+          )}
+          {id === post.user._id && profilePosts && (
+            <div className='tag-user-btn-div'>
+              <button
+                className='tag-user-btn'
+                aria-label='click to tag a user in this image'
+                onClick={() => setShowTagUsersModal(true)}
+              >
+                <img src={tagUserIcon} alt='tag user icon' />
+              </button>
+            </div>
+          )}
+          <div className='icon-div'>
+            <div className='icon-row'>
+              <img src={heart} alt='heart icon' />
+              <span>{post.likes}</span>
+            </div>
+            <div className='icon-row'>
+              <img src={message} alt='message bubble icon' />
+              <span>{post?.comments || 0}</span>
+            </div>
           </div>
-        )}
-        {id === post.user._id && profilePosts && (
-          <div className='tag-user-btn-div'>
-            <button
-              className='tag-user-btn'
-              aria-label='click to tag a user in this image'
-              onClick={() => setShowTagUsersModal(true)}
-            >
-              <img src={tagUserIcon} alt='tag user icon' />
-            </button>
-          </div>
-        )}
-        <div className='icon-div'>
-          <div className='icon-row'>
-            <img src={heart} alt='heart icon' />
-            <span>{post.likes}</span>
-          </div>
-          <div className='icon-row'>
-            <img src={message} alt='message bubble icon' />
-            <span>{post?.comments || 0}</span>
-          </div>
+          <div
+            className='background'
+            onClick={() => setShowPhotoModal(true)}
+          ></div>
         </div>
-        <div
-          className='background'
-          onClick={() => setShowPhotoModal(true)}
-        ></div>
-      </div>
-      {showDeleteConfirmation && (
-        <DeletePostConfirmationModal
-          setShowDeleteConfirmation={setShowDeleteConfirmation}
-          post={post}
-        />
-      )}
-      {showPhotoModal && (
-        <PhotoModal setShowPhotoModal={setShowPhotoModal} post={post} />
-      )}
-      {showTagUsersModal && (
-        <TagUsersModal
-          post={post}
-          setShowTagUsersModal={setShowTagUsersModal}
-        />
-      )}
-    </>
-  );
+        {showDeleteConfirmation && (
+          <DeletePostConfirmationModal
+            setShowDeleteConfirmation={setShowDeleteConfirmation}
+            post={post}
+          />
+        )}
+        {showPhotoModal && (
+          <PhotoModal
+            setShowPhotoModal={setShowPhotoModal}
+            post={post}
+            queryKey={queryKey}
+          />
+        )}
+        {showTagUsersModal && (
+          <TagUsersModal
+            post={post}
+            setShowTagUsersModal={setShowTagUsersModal}
+          />
+        )}
+      </>
+    );
 
-  const postCard = ref ? (
-    <article
-      aria-label='click to see image and comments'
-      className='post-card-container'
-      ref={ref}
-    >
-      {cardContent}
-    </article>
-  ) : (
-    <article
-      aria-label='click to see image and comments'
-      className='post-card-container'
-    >
-      {cardContent}
-    </article>
-  );
+    const postCard = ref ? (
+      <article
+        aria-label='click to see image and comments'
+        className='post-card-container'
+        ref={ref}
+      >
+        {cardContent}
+      </article>
+    ) : (
+      <article
+        aria-label='click to see image and comments'
+        className='post-card-container'
+      >
+        {cardContent}
+      </article>
+    );
 
-  return postCard;
-});
+    return postCard;
+  }
+);
 
 export default ProfilePostCard;
