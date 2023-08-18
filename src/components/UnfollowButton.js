328d83cf5309e83ded4b72b186dc46d54849f089
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-18 18:15:15 +0900

    Set query data for followers & following upon success

--- src/components/UnfollowButton.js
+++ src/components/UnfollowButton.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { deleteFollow } from "../features/follow/followApiRoutes";
 import useAuth from "../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-import { FadeLoader } from "react-spinners";
+import { useNavigate, useParams } from "react-router-dom";
 
-const UnfollowButton = ({ user, queryKey }) => {
-  const { authenticatedUser, id } = useAuth();
+const UnfollowButton = ({ user }) => {
+  const { authenticatedUser, id, username } = useAuth();
+
+  const { userID: usernameKey } = useParams();
 
   const queryClient = useQueryClient();
   const navigate = useNavigate();
@@ -17,7 +18,6 @@ const UnfollowButton = ({ user, queryKey }) => {
       queryClient.setQueryData(["userInfo", user.username], (oldData) => {
         if (oldData) {
           const data = oldData;
-          console.log(data);
           data.isFollow = false;
           data.followerNo = data.followerNo -= 1;
           return data;
@@ -26,10 +26,12 @@ const UnfollowButton = ({ user, queryKey }) => {
       queryClient.invalidateQueries({
         queryKey: ["userInfo", user.username],
       });
+      queryClient.invalidateQueries({
+        queryKey: ["userInfo", username],
+      });
       queryClient.setQueryData(["posts"], (oldData) => {
         if (oldData) {
           const data = oldData;
-          console.log(data);
           for (const page of data.pages) {
             for (const post of page.posts) {
               if (post.user._id === user._id) {
@@ -37,7 +39,32 @@ const UnfollowButton = ({ user, queryKey }) => {
               }
             }
           }
-          console.log(data);
+          return data;
+        }
+      });
+      queryClient.setQueryData(["following", usernameKey], (oldData) => {
+        if (oldData) {
+          const data = oldData;
+          for (const page of data.pages) {
+            for (const following of page.following) {
+              if (following.following._id === user._id) {
+                following.following.isFollow = false;
+              }
+            }
+          }
+          return data;
+        }
+      });
+      queryClient.setQueryData(["followers", usernameKey], (oldData) => {
+        if (oldData) {
+          const data = oldData;
+          for (const page of data.pages) {
+            for (const follower of page.followers) {
+              if (follower.follower._id === user._id) {
+                follower.follower.isFollow = false;
+              }
+            }
+          }
           return data;
         }
       });
