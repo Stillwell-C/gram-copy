18ece44638fcf7b4108cceb9a0be61206df63d2f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-25 18:25:43 +0900

    Update to use react-query

--- src/components/deleteAccountModal/DeleteAccountModal.js
+++ src/components/deleteAccountModal/DeleteAccountModal.js
@@ -1,40 +1,54 @@
-import {
-  deleteUser,
-  EmailAuthProvider,
-  reauthenticateWithCredential,
-} from "firebase/auth";
-import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/authContext";
-import { auth, db } from "../../firebase";
-import useGetLoggedInUserInfoFunction from "../../hooks/useGetLoggedInUserInfoFunction";
-import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import "./deleteAccountModal.scss";
+import { useMutation, useQueryClient } from "react-query";
+import useAuth from "../../hooks/useAuth";
+import { FadeLoader } from "react-spinners";
+import { useSendLogoutMutation } from "../../features/auth/authApiSlice";
+import { deleteUser } from "../../features/users/usersApiRoutes";
 
 const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
-  const { currentUser } = useContext(AuthContext);
+  const { id } = useAuth();
 
   const navigate = useNavigate();
+  const errRef = useRef(null);
 
   const [checkbox, setCheckbox] = useState(false);
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
-  const [confirmation, setConfirmation] = useState(false);
-  const [confirmationMsg, setConfirmationMsg] = useState("");
-  const [userData, setUserData] = useState({});
-  const [loading, setLoading] = useState(false);
 
-  const getUserInfo = useGetLoggedInUserInfoFunction();
+  const [
+    sendLogout,
+    {
+      isLoading: logoutLoading,
+      isSuccess: logoutSuccess,
+      isError: logoutIsError,
+      error: logoutError,
+    },
+  ] = useSendLogoutMutation();
+
+  const queryClient = useQueryClient();
+
+  const deleteUserMutation = useMutation({
+    mutationFn: deleteUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+      sendLogout();
+    },
+  });
 
   useEffect(() => {
-    const fetchAndSetData = async () => {
-      const userInfo = await getUserInfo();
-      setUserData(userInfo);
-    };
-    fetchAndSetData();
-  }, []);
+    if (logoutSuccess) {
+      navigate("/");
+    }
+  }, [logoutSuccess]);
+
+  useEffect(() => {
+    if (logoutIsError || deleteUserMutation.isError || error) {
+      errRef.current.focus();
+    }
+  }, [logoutIsError, deleteUserMutation.isError, error]);
 
   const handleClose = () => {
     setDisplayDeleteModal(false);
@@ -42,57 +56,22 @@ const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
 
   const deleteAccount = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setConfirmation(false);
-    setConfirmationMsg("");
     setError(false);
     setErrorMsg("");
     if (!checkbox) {
       setError(true);
       setErrorMsg("You must agree to the terms of service");
-      setLoading(false);
       return;
     }
     if (!password.length) {
       setError(true);
       setErrorMsg("Please input your password");
-      setLoading(false);
       return;
     }
-    if (!userData.userPosts) {
-      setError(true);
-      setErrorMsg("Error retrieving user information. Please try again.");
-      setLoading(false);
-      return;
-    }
-    const user = auth.currentUser;
-    try {
-      const credential = EmailAuthProvider.credential(
-        currentUser.email,
-        password
-      );
-      await reauthenticateWithCredential(user, credential);
-      for (let post of userData.userPosts) {
-        console.log(post);
-        const fetchedPost = await getDoc(doc(db, "userImgs", post));
-        const postData = fetchedPost.data();
-        await setDoc(doc(db, "deletedUserPosts", post), { ...postData });
-        await deleteDoc(doc(db, "userImgs", post));
-      }
-      await setDoc(doc(db, "deletedUsers", currentUser.uid), { ...userData });
-      await deleteDoc(doc(db, "userInfo", currentUser.uid));
-      await deleteUser(user);
-      setConfirmation(true);
-      setConfirmationMsg("Your account has been deleted");
-      setLoading(false);
-      navigate("/");
-    } catch (err) {
-      console.log(err.message);
-      console.log(err.code);
-      setError(true);
-      setErrorMsg(err.code);
-      setLoading(false);
-    }
+    deleteUserMutation.mutate({
+      userID: id,
+      userPassword: password,
+    });
   };
 
   return (
@@ -105,13 +84,16 @@ const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
           <div className='modal-body'>
             <div
               className={
-                confirmation || error
+                deleteUserMutation.isError || error
                   ? "user-msg-container active"
                   : "user-msg-container"
               }
             >
-              <div className='error-msg'>{errorMsg}</div>
-              <div className='confirmation-msg'>{confirmationMsg}</div>
+              <div className='error-msg' ref={errRef}>
+                {deleteUserMutation?.error?.response?.data?.message}
+                {logoutError?.message}
+                {errorMsg}
+              </div>
             </div>
             <h3 className='modal-body-header'>
               Are you sure you want to delete your account?
@@ -122,7 +104,7 @@ const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
                 <div className='fine-print-div'>
                   <span className='fine-print'>
                     {
-                      "By opting to delete their account and clicking the below 'Delete account' button the user of this account (hereinafter \"User\") is agreeing to Instagram's terms of service. By deleting their account, User forfeits any and all rights to any information, data, images, etc. (hereinafter \"User Data\") that has been disclosed, provided, or uploaded to Instagram and relinquishes to Instagram the right to use and retain the User Data as well as User's image and likeness for any means including but not limited to commercial purposes in perpetuity across and throughout this or any other universe. Furthermore, by deleting this account, User knowingly and voluntarily forfeits any rights, protections, or privileges granted, provided, or guaranteed by any local, national, or international statute or law; international custom; multi-state, international, or United Nations treaty, pact, or convention; maritime law; or general principles of law with respect to the User Data and User's image and likeness, use and monetization of the User Data and User's image and likeness, and any objections thereto. Further, by deleting this account, User knowingly and voluntarily forfeits any right to make any legal objection to any provision herein or take any legal action against Instagram through local, national, or international legal systems and agrees to settle any and all objections through a private third-party mediator, provided that said mediator shall be selected by Instagram and any monetary compensation such as fees, expenses, or wages required by said mediator shall be paid in full by User. Further, should any legal action be taken against Instagram by User, User knowingly and voluntarily agrees to pay in full any and all legal expenses incurred by Instagram in relation to said legal action regardless of the outcome of said legal action."
+                      "By opting to delete this account and clicking the below 'Delete account' button the user of this account (hereinafter \"User\") hereby agrees to Instagram's terms of service. By deleting this account, User forfeits any and all rights to any information, data, images, etc. (hereinafter \"User Data\") that has been disclosed, provided, or uploaded to Instagram and relinquishes to Instagram the right to use and retain User Data as well as User's image and likeness for any means including but not limited to commercial purposes in perpetuity across and throughout this or any other universe. Furthermore, by deleting this account, User knowingly and voluntarily forfeits any rights, protections, or privileges granted, provided, or guaranteed by any local, national, or international statute or law; international custom; multi-state, international, or United Nations treaty, pact, protocol, convention, etc.; maritime law; or general principle of law with respect to User Data and User's image and likeness, use and monetization of User Data and User's image and likeness, retention in perpetuity of User Data and User's image and likeness, and use of User Data and User's image and likeness by a computer(s) or computer system(s), including artificial intelligence, or physical beings including human beings for any means including but not limited to commercial purposes, including but not limited to the editing of User Data and User's image and likeness and use of edited User Data and User's image and likeness; the creation, synthesis, use, etc. of User Data and User's image and likeness for purposes including but not limited to the creation of new users or user data; etc. for any use including for commercial and promotional uses. Further, by deleting this account, User knowingly and voluntarily forfeits any right to make any legal objection to any provision herein or take any legal action against Instagram through local, national, or international legal systems and agrees to settle any and all objections through a private third-party mediator, provided that the mediator shall be selected by Instagram and any monetary compensation such as fees, expenses, or wages required by the mediator shall be paid in full by User regardless of the decision of the mediator. Further, should any legal action be taken against Instagram by User, User knowingly and voluntarily agrees to pay in full any and all legal expenses incurred by Instagram in relation to this legal action regardless of the outcome of the legal action."
                     }
                   </span>
                 </div>
@@ -154,7 +136,7 @@ const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
                     />
                   </label>
                 </div>
-                {!loading && (
+                {!deleteUserMutation.isLoading && (
                   <div className='button-div'>
                     <button
                       className='delete-button'
@@ -165,10 +147,15 @@ const DeleteAccountModal = ({ setDisplayDeleteModal }) => {
                     </button>
                   </div>
                 )}
-                {loading && (
+                {deleteUserMutation.isLoading && (
                   <div className='delete-loading-div'>
                     <div className='loading-spinner-div'>
-                      <LoadingSpinner />
+                      <div className='loading-div'>
+                        <FadeLoader
+                          color='#333'
+                          cssOverride={{ scale: "0.7" }}
+                        />
+                      </div>
                     </div>
                     <p>Deleting Account...</p>
                     <p>Do not refresh or close page.</p>
