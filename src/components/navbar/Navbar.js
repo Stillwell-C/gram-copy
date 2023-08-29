91f087bae7b0d17d56846f67edea8bff899244f0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-29 18:15:29 +0900

    Add SideNavbar and SideNavbarSearch components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -29,6 +29,8 @@ import { getUserSearch } from "../../features/users/usersApiRoutes";
 import { useInfiniteQuery } from "react-query";
 import { FadeLoader } from "react-spinners";
 import SearchResult from "../SearchResult";
+import SideNavbar from "../SideNavbar";
+import SideNavbarSearch from "../SideNavbarSearch";
 
 const Navbar = () => {
   const [displayPostModal, setDisplayPostModal] = useState(false);
@@ -47,25 +49,25 @@ const Navbar = () => {
   const [sendLogout, { isLoading, isSuccess, isError, error }] =
     useSendLogoutMutation();
 
-  const {
-    data: searchData,
-    isLoading: searchIsLoading,
-    isError: searchIsError,
-    error: searchError,
-    isFetching: searchIsFetching,
-    hasNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["searchData", searchQuery],
-    queryFn: ({ pageParam = 1 }) =>
-      getUserSearch({ pageParam, limit: 10, searchQuery }),
-    refetchOnWindowFocus: false,
-    enabled: searchQuery.length > 0,
-    getNextPageParam: (lastPage, pages) => {
-      if (lastPage.page < lastPage.totalPages) return lastPage.page + 1;
-      return false;
-    },
-  });
+  // const {
+  //   data: searchData,
+  //   isLoading: searchIsLoading,
+  //   isError: searchIsError,
+  //   error: searchError,
+  //   isFetching: searchIsFetching,
+  //   hasNextPage,
+  //   fetchNextPage,
+  // } = useInfiniteQuery({
+  //   queryKey: ["searchData", searchQuery],
+  //   queryFn: ({ pageParam = 1 }) =>
+  //     getUserSearch({ pageParam, limit: 10, searchQuery }),
+  //   refetchOnWindowFocus: false,
+  //   enabled: searchQuery.length > 0,
+  //   getNextPageParam: (lastPage, pages) => {
+  //     if (lastPage.page < lastPage.totalPages) return lastPage.page + 1;
+  //     return false;
+  //   },
+  // });
 
   useEffect(() => {
     if (isSuccess) window.location.reload();
@@ -89,16 +91,16 @@ const Navbar = () => {
     }
   };
 
-  const handleSearch = async (searchInput) => {
-    if (searchInput > 1) {
-      return;
-    }
-    setSearchQuery(searchInput);
-  };
+  // const handleSearch = async (searchInput) => {
+  //   if (searchInput > 1) {
+  //     return;
+  //   }
+  //   setSearchQuery(searchInput);
+  // };
 
-  const handleClearSearch = () => {
-    setSearchQuery("");
-  };
+  // const handleClearSearch = () => {
+  //   setSearchQuery("");
+  // };
 
   useEffect(() => {
     let handler = (e) => {
@@ -118,42 +120,147 @@ const Navbar = () => {
     };
   });
 
-  const observer = useRef();
-  const lastResultRef = useCallback(
-    (post) => {
-      if (searchIsFetching || searchIsLoading) return;
-      if (observer.current) observer.current.disconnect();
+  // const observer = useRef();
+  // const lastResultRef = useCallback(
+  //   (post) => {
+  //     if (searchIsFetching || searchIsLoading) return;
+  //     if (observer.current) observer.current.disconnect();
 
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasNextPage) {
-          console.log("has more pages: ");
-          fetchNextPage();
-          console.log("near last result");
-        }
-      });
+  //     observer.current = new IntersectionObserver((entries) => {
+  //       if (entries[0].isIntersecting && hasNextPage) {
+  //         console.log("has more pages: ");
+  //         fetchNextPage();
+  //         console.log("near last result");
+  //       }
+  //     });
 
-      if (post) observer.current.observe(post);
-    },
-    [searchIsFetching, searchIsLoading, hasNextPage]
-  );
+  //     if (post) observer.current.observe(post);
+  //   },
+  //   [searchIsFetching, searchIsLoading, hasNextPage]
+  // );
 
   const userImgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_150,c_fill/${img}`;
 
-  const flattenedSearchData = searchData?.pages?.reduce((acc, page) => {
-    return [...acc, ...page?.users];
-  }, []);
+  // const flattenedSearchData = searchData?.pages?.reduce((acc, page) => {
+  //   return [...acc, ...page?.users];
+  // }, []);
 
-  const searchResults = flattenedSearchData?.map((user, i) => {
-    if (flattenedSearchData.length === i + 1) {
-      return <SearchResult ref={lastResultRef} key={user._id} user={user} />;
-    }
-    return <SearchResult key={user._id} user={user} />;
-  });
+  // const searchResults = flattenedSearchData?.map((user, i) => {
+  //   if (flattenedSearchData.length === i + 1) {
+  //     return <SearchResult ref={lastResultRef} key={user._id} user={user} />;
+  //   }
+  //   return <SearchResult key={user._id} user={user} />;
+  // });
+
+  const homeLink = (
+    <Link to='/'>
+      <div className='navbar-line'>
+        <img src={home} alt='' aria-hidden='true' />
+        <span>Home</span>
+      </div>
+    </Link>
+  );
+
+  const searchLink = (
+    <Link to='#'>
+      <div
+        className='navbar-line'
+        onClick={() => {
+          setSearchActive(!searchActive);
+        }}
+      >
+        <img src={search} alt='' aria-hidden='true' />
+        <span>Search</span>
+      </div>
+    </Link>
+  );
+
+  const exploreLink = (
+    <Link to='#'>
+      <div className='navbar-line'>
+        <img src={compass} alt='' aria-hidden='true' />
+        <span>Explore</span>
+      </div>
+    </Link>
+  );
+
+  const messagesLink = (
+    <Link to='/direct/inbox'>
+      <div className='navbar-line'>
+        <img src={message} alt='' aria-hidden='true' />
+        <span>Messages</span>
+      </div>
+    </Link>
+  );
+
+  const notificationsLink = (
+    <Link to='#'>
+      <div className='navbar-line'>
+        <img src={heart} alt='' aria-hidden='true' />
+        <span>Notifications</span>
+      </div>
+    </Link>
+  );
+
+  const createLink = (
+    <Link to='#'>
+      <div className='navbar-line'>
+        <img src={add} alt='' aria-hidden='true' />
+        <span>Create</span>
+      </div>
+    </Link>
+  );
+
+  const profileLink = (
+    <Link to={authenticatedUser ? `/${username}` : "/accounts/login"}>
+      <div className='navbar-line'>
+        <img
+          src={authenticatedUser ? userImgURL : profile}
+          className={authenticatedUser ? "userProfileImg" : ""}
+          alt=''
+          aria-hidden='true'
+        />
+        <span>Profile</span>
+      </div>
+    </Link>
+  );
+
+  const moreLink = (
+    <Link
+      to='#'
+      aria-label='click to see more options'
+      aria-expanded={displayMenu ? "true" : "false"}
+      aria-controls='large-navbar-menu-content'
+    >
+      <div className='navbar-line' onClick={() => setDisplayMenu(!displayMenu)}>
+        <img src={menu} alt='menu icon' />
+        <span>More</span>
+      </div>
+    </Link>
+  );
+
+  const menuContent = (
+    <>
+      <div className='menu-line'>
+        <button aria-label='click to change to dark mode'>
+          <span>Change appearance</span>
+          <img src={moon} alt='' aria-hidden='true' />
+        </button>
+      </div>
+      <div className='menu-line'>
+        {authenticatedUser ? (
+          <button onClick={handleLogout}>Log out</button>
+        ) : (
+          <Link to='/accounts/login'>Log in</Link>
+        )}
+      </div>
+    </>
+  );
 
   return (
     <>
       <HeaderBar
-        handleSearch={handleSearch}
+        // handleSearch={handleSearch}
         searchActive={searchActive}
         setSearchActive={setSearchActive}
         searchQuery={searchQuery}
@@ -264,7 +371,7 @@ const Navbar = () => {
         ref={navbarRef}
         aria-label='main navigation bar'
       >
-        <div className='navbar-body'>
+        {/* <div className='navbar-body'>
           <div className='navbar-top'>
             <div className='navbar-header'>
               <Link to='/'>
@@ -393,8 +500,21 @@ const Navbar = () => {
               onClick={() => setDisplayMenu(false)}
             ></div>
           </div>
-        </div>
-        <div className='search-body'>
+        </div> */}
+        <SideNavbar
+          homeLink={homeLink}
+          searchLink={searchLink}
+          exploreLink={exploreLink}
+          messagesLink={messagesLink}
+          notificationsLink={notificationsLink}
+          createLink={createLink}
+          profileLink={profileLink}
+          moreLink={moreLink}
+          menuContent={menuContent}
+          displayMenu={displayMenu}
+          handleAddPostModal={handleAddPostModal}
+        />
+        {/* <div className='search-body'>
           <div className='search-top'>
             <div className='search-header-div'>
               <h2>Search</h2>
@@ -434,7 +554,16 @@ const Navbar = () => {
               </div>
             )}
           </div>
-        </div>
+        </div> */}
+        <SideNavbarSearch />
+        {/* <div
+          className={
+            displayMenu
+              ? "navbar-menu-close-field active"
+              : "navbar-menu-close-field"
+          }
+          onClick={() => setDisplayMenu(false)}
+        ></div> */}
       </nav>
       {displayPostModal && (
         <CreatePostModal setDisplayPostModal={setDisplayPostModal} />
