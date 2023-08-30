80a94049898eb698bd2bca5011f841faef0cd0b2
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-30 15:20:39 +0900

    Move navbar logic to navbar component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
-import Navbar from "./navbar/Navbar";
+import { Outlet } from "react-router-dom";
 import styles from "../scss/layout.module.scss";
 import "../scss/global.scss";
 import { useSelector } from "react-redux";
@@ -7,26 +6,13 @@ import { selectLoadingState } from "../features/display/displaySlice";
 import LoadingFullPage from "./LoadingFullPage";
 
 const Layout = () => {
-  //Use this to not display navbar on pages
-  const { pathname } = useLocation();
   const loadingState = useSelector(selectLoadingState);
-
-  let displayNav = true;
-  if (
-    pathname.match(/\/accounts\/login/i) ||
-    pathname.match(/\/accounts\/emailsignup/i)
-  ) {
-    displayNav = false;
-  }
 
   return (
     <>
       <LoadingFullPage loadingState={loadingState} />
       <div className={styles.layout}>
-        {displayNav && <Navbar />}
-        <div className={styles.outlet}>
-          <Outlet />
-        </div>
+        <Outlet />
       </div>
     </>
   );
