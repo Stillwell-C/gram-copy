32721a2991b92a4dbbcea8d3bccde11c2efc28fd
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-08-30 11:15:45 +0900

    Initial commit

---
+++ src/components/FooterNavbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const FooterNavbar = ({
+  homeLink,
+  exploreLink,
+  messagesLink,
+  notificationsLink,
+  createLink,
+  profileLink,
+  moreLink,
+  menuContent,
+  displayMenu,
+  handleAddPostModal,
+  setDisplayMenu,
+}) => {
+  return (
+    <nav className='navbar-container-bottom' aria-label='bottom navigation bar'>
+      <div className='navbar-row'>{homeLink}</div>
+      <div className='navbar-row'>{messagesLink}</div>
+      <div className='navbar-row'>{notificationsLink}</div>
+      <div className='navbar-row' onClick={handleAddPostModal}>
+        {createLink}
+      </div>
+      <div className='navbar-row'>{profileLink}</div>
+      <div className='navbar-row'>{moreLink}</div>
+      <div
+        className='navbar-menu-container'
+        id='small-navbar-menu-content'
+        aria-hidden={!displayMenu ? "true" : "false"}
+      >
+        <div className={displayMenu ? "navbar-menu active" : "navbar-menu"}>
+          {menuContent}
+        </div>
+        <div
+          className={
+            displayMenu
+              ? "navbar-menu-close-field active"
+              : "navbar-menu-close-field"
+          }
+          onClick={() => setDisplayMenu(false)}
+        ></div>
+      </div>
+    </nav>
+  );
+};
+
+export default FooterNavbar;
