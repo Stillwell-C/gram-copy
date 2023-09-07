162c96f62530ce7cfafa85a227092656dfc65900
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-07 12:22:19 +0900

    Add footer & page styles

--- src/components/NotificationsPage.js
+++ src/components/NotificationsPage.js
@@ -6,6 +6,7 @@ import Notification from "./Notification";
 import addNotificationsIcon from "../assets/add-circle-svgrepo-com.svg";
 import { FadeLoader } from "react-spinners";
 import "../scss/notification.scss";
+import Footer from "./footer/Footer";
 
 const NotificationsPage = () => {
   const navigate = useNavigate();
@@ -53,27 +54,32 @@ const NotificationsPage = () => {
   });
 
   return (
-    <div className='flex-container fg-1 flex-column flex-align-center margin-top-3 gap-2'>
-      <h2>Notifications</h2>
-      {content}
-      {isError && error?.response?.data?.message}
-      {(isLoading || isFetching) && (
-        <FadeLoader
-          cssOverride={{ alignSelf: "center", scale: "0.5" }}
-          color='#333'
-        />
-      )}
-      {hasNextPage && !isLoading && !isFetching && (
-        <div className='add-notifications-button-div'>
-          <button
-            type='button'
-            aria-label='show more comments'
-            onClick={fetchNextPage}
-          >
-            <img src={addNotificationsIcon} alt='' aria-hidden='true' />
-          </button>
-        </div>
-      )}
+    <div className='flex-container fg-1 flex-column flex-align-center margin-top-3 notifications-page'>
+      <h2 className='margin-btm-2'>Notifications</h2>
+      <div className='notifications-content flex-container fg-1 flex-column flex-align-center gap-2'>
+        {content}
+        {isError && error?.response?.data?.message}
+        {(isLoading || isFetching) && (
+          <FadeLoader
+            cssOverride={{ alignSelf: "center", scale: "0.5" }}
+            color='#333'
+          />
+        )}
+        {hasNextPage && !isLoading && !isFetching && (
+          <div className='add-notifications-button-div'>
+            <button
+              type='button'
+              aria-label='show more comments'
+              onClick={fetchNextPage}
+            >
+              <img src={addNotificationsIcon} alt='' aria-hidden='true' />
+            </button>
+          </div>
+        )}
+      </div>
+      <div className='footer-container margin-top-2'>
+        <Footer />
+      </div>
     </div>
   );
 };
