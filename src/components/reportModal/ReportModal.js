ff3ddb344c55a89edfe47ae6061a651e6484f849
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-08 17:08:22 +0900

    Return modal content inside of focus trap parent

--- src/components/reportModal/ReportModal.js
+++ src/components/reportModal/ReportModal.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import "./reportModal.scss";
 import { useMutation } from "react-query";
 import { addReport } from "../../features/reports/reportsApiRoutes";
+import FocusTrapModalParent from "../FocusTrapModalParent";
 
 const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
   const [error, setError] = useState(false);
@@ -85,108 +86,117 @@ const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
     setReportReason("inappropriate");
   };
 
-  return (
-    <>
-      <div className='report-modal-container'>
-        <div className='report-modal-body'>
-          <div className='modal-header'>
-            <div></div>
-            <div className='header-text-div'>
-              <h2>
-                {`Report ${
-                  reportDistinction.slice(0, 1).toUpperCase() +
-                  reportDistinction.slice(1)
-                }`}
-              </h2>
-            </div>
-            <div className='close-div'>
-              <button
-                onClick={() => setShowReportModal(false)}
-                aria-label='click to close'
-              >
-                &times;
-              </button>
-            </div>
+  const handleClose = () => {
+    setShowReportModal(false);
+  };
+
+  const content = (
+    <div
+      className='report-modal-container'
+      role='dialog'
+      aria-labelledby='dialog-header'
+    >
+      <div className='report-modal-body'>
+        <div className='modal-header'>
+          <div></div>
+          <div className='header-text-div'>
+            <h2 id='dialog-header'>
+              {`Report ${
+                reportDistinction.slice(0, 1).toUpperCase() +
+                reportDistinction.slice(1)
+              }`}
+            </h2>
           </div>
-          <div className='modal-form'>
-            <form onSubmit={handleSubmit}>
-              {error && (
-                <div className='error-div' ref={errRef}>
-                  {errMsg}
-                </div>
-              )}
-              <div className='reason-div'>
-                <h4>Why are you reporting this {reportDistinction}?</h4>
-                <ul>
-                  <li className='button-li'>
-                    <button
-                      type='button'
-                      onClick={handleScamSpam}
-                      className={`${reportReason === "spam/scam" && "active"}`}
-                    >
-                      Spam or Scam
-                    </button>
-                  </li>
-                  <li className='button-li'>
-                    <button
-                      type='button'
-                      onClick={handleHatefulAbusive}
-                      className={`${
-                        reportReason === "hateful/abusive" && "active"
-                      }`}
-                    >
-                      Hateful or Abusive Content
-                    </button>
-                  </li>
-                  <li className='button-li'>
-                    <button
-                      type='button'
-                      onClick={handleInappropriate}
-                      className={`${
-                        reportReason === "inappropriate" && "active"
-                      }`}
-                    >
-                      Inappropriate Content
-                    </button>
-                  </li>
-                </ul>
-              </div>
-              <div className='textarea-div'>
-                <h4 className='textarea-header'>
-                  {"Additional comments (optional)"}
-                </h4>
-                <textarea
-                  id='additional-comments'
-                  name='additional-comments'
-                  className='additional-comments-textarea'
-                  value={additionalComments}
-                  onChange={(e) => {
-                    setAdditionalComments(e.target.value);
-                  }}
-                  maxLength='2200'
-                  placeholder='Write additional comments...'
-                  aria-label='Add additional comments. Max length 2200 characters.'
-                ></textarea>
-                <span
-                  className='textarea-count'
-                  aria-label='length of additional comments'
-                >
-                  {additionalComments.length} / 2200
-                </span>
-              </div>
-              <button className='submit-button' type='submit'>
-                Submit Report
-              </button>
-            </form>
+          <div className='close-div'>
+            <button
+              onClick={() => setShowReportModal(false)}
+              aria-label='click to close'
+            >
+              &times;
+            </button>
           </div>
         </div>
+        <div className='modal-form'>
+          <form onSubmit={handleSubmit}>
+            {error && (
+              <div className='error-div' ref={errRef}>
+                {errMsg}
+              </div>
+            )}
+            <div className='reason-div'>
+              <h4>Why are you reporting this {reportDistinction}?</h4>
+              <ul>
+                <li className='button-li'>
+                  <button
+                    type='button'
+                    onClick={handleScamSpam}
+                    className={`${reportReason === "spam/scam" && "active"}`}
+                  >
+                    Spam or Scam
+                  </button>
+                </li>
+                <li className='button-li'>
+                  <button
+                    type='button'
+                    onClick={handleHatefulAbusive}
+                    className={`${
+                      reportReason === "hateful/abusive" && "active"
+                    }`}
+                  >
+                    Hateful or Abusive Content
+                  </button>
+                </li>
+                <li className='button-li'>
+                  <button
+                    type='button'
+                    onClick={handleInappropriate}
+                    className={`${
+                      reportReason === "inappropriate" && "active"
+                    }`}
+                  >
+                    Inappropriate Content
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className='textarea-div'>
+              <h4 className='textarea-header'>
+                {"Additional comments (optional)"}
+              </h4>
+              <textarea
+                id='additional-comments'
+                name='additional-comments'
+                className='additional-comments-textarea'
+                value={additionalComments}
+                onChange={(e) => {
+                  setAdditionalComments(e.target.value);
+                }}
+                maxLength='2200'
+                placeholder='Write additional comments...'
+                aria-label='Add additional comments. Max length 2200 characters.'
+              ></textarea>
+              <span
+                className='textarea-count'
+                aria-label='length of additional comments'
+              >
+                {additionalComments.length} / 2200
+              </span>
+            </div>
+            <button className='submit-button' type='submit'>
+              Submit Report
+            </button>
+          </form>
+        </div>
       </div>
-      <div
-        className='report-modal-overlay'
-        aria-label='click to close modal'
-        onClick={() => setShowReportModal(false)}
-      ></div>
-    </>
+    </div>
+  );
+
+  return (
+    <FocusTrapModalParent
+      content={content}
+      handleClose={handleClose}
+      showClose={false}
+    />
   );
 };
 
