de00e66a3c7b9cf3ce234484484fb352c93a9209
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-08 12:04:17 +0900

    Update to use react-query

--- src/components/reportModal/ReportModal.js
+++ src/components/reportModal/ReportModal.js
@@ -1,7 +1,7 @@
-import { collection, doc, serverTimestamp, setDoc } from "firebase/firestore";
-import React, { useState } from "react";
-import { db } from "../../firebase";
+import React, { useRef, useState } from "react";
 import "./reportModal.scss";
+import { useMutation } from "react-query";
+import { addReport } from "../../features/reports/reportsApiRoutes";
 
 const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
   const [error, setError] = useState(false);
@@ -9,12 +9,29 @@ const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
   const [reportReason, setReportReason] = useState(null);
   const [additionalComments, setAdditionalComments] = useState("");
 
+  const errRef = useRef();
+
+  const submitReportMutation = useMutation({
+    mutationFn: addReport,
+    onSuccess: () => {
+      setReportReason(null);
+      setAdditionalComments("");
+      setShowReportModal(false);
+    },
+    onError: () => {
+      setError(true);
+      setErrMsg(submitReportMutation?.error?.response?.data?.message);
+      errRef.current.focus();
+    },
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
+    setErrMsg("");
     if (!reportReason) {
       setError(true);
-      setErrMsg("Please choose a reason for making a report");
+      setErrMsg("Please select a reason for the report");
       return;
     }
     if (additionalComments.length > 2200) {
@@ -23,20 +40,49 @@ const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
       return;
     }
     const reportContent = {
-      reportType: reportDistinction,
-      reportId: reportId,
+      reportType: reportDistinction.toUpperCase(),
       reportReason: reportReason,
       additionalComments: additionalComments,
-      date: serverTimestamp(),
     };
-
-    try {
-      await setDoc(doc(collection(db, "reports")), reportContent);
-      setShowReportModal(false);
-    } catch (err) {
-      setError(true);
-      setErrMsg(err.code);
+    if (reportDistinction.match(/post/i)) {
+      reportContent.reportedPost = reportId;
     }
+    if (reportDistinction.match(/user/i)) {
+      reportContent.reportedUser = reportId;
+    }
+    submitReportMutation.mutate(reportContent);
+
+    // try {
+    //   await setDoc(doc(collection(db, "reports")), reportContent);
+    //   setShowReportModal(false);
+    // } catch (err) {
+    //   setError(true);
+    //   setErrMsg(err.code);
+    // }
+  };
+
+  const handleScamSpam = () => {
+    if (reportReason === "spam/scam") {
+      setReportReason("");
+      return;
+    }
+    setReportReason("spam/scam");
+  };
+
+  const handleHatefulAbusive = () => {
+    if (reportReason === "hateful/abusive") {
+      setReportReason("");
+      return;
+    }
+    setReportReason("hateful/abusive");
+  };
+
+  const handleInappropriate = () => {
+    if (reportReason === "inappropriate") {
+      setReportReason("");
+      return;
+    }
+    setReportReason("inappropriate");
   };
 
   return (
@@ -64,56 +110,70 @@ const ReportModal = ({ setShowReportModal, reportDistinction, reportId }) => {
           </div>
           <div className='modal-form'>
             <form onSubmit={handleSubmit}>
-              <div className='error-div'>{error && errMsg}</div>
+              {error && (
+                <div className='error-div' ref={errRef}>
+                  {errMsg}
+                </div>
+              )}
               <div className='reason-div'>
                 <h4>Why are you reporting this {reportDistinction}?</h4>
-                <div className='button-div'>
-                  <button
-                    type='button'
-                    onClick={() => setReportReason("spam/scam")}
-                    className={`${reportReason === "spam/scam" && "active"}`}
-                  >
-                    Spam or Scam
-                  </button>
-                </div>
-                <div className='button-div'>
-                  <button
-                    type='button'
-                    onClick={() => setReportReason("hateful/abusive")}
-                    className={`${
-                      reportReason === "hateful/abusive" && "active"
-                    }`}
-                  >
-                    Hateful or Abusive Content
-                  </button>
-                </div>
-                <div className='button-div'>
-                  <button
-                    type='button'
-                    onClick={() => setReportReason("inappropriate")}
-                    className={`${
-                      reportReason === "inappropriate" && "active"
-                    }`}
-                  >
-                    Inappropriate Content
-                  </button>
-                </div>
+                <ul>
+                  <li className='button-li'>
+                    <button
+                      type='button'
+                      onClick={handleScamSpam}
+                      className={`${reportReason === "spam/scam" && "active"}`}
+                    >
+                      Spam or Scam
+                    </button>
+                  </li>
+                  <li className='button-li'>
+                    <button
+                      type='button'
+                      onClick={handleHatefulAbusive}
+                      className={`${
+                        reportReason === "hateful/abusive" && "active"
+                      }`}
+                    >
+                      Hateful or Abusive Content
+                    </button>
+                  </li>
+                  <li className='button-li'>
+                    <button
+                      type='button'
+                      onClick={handleInappropriate}
+                      className={`${
+                        reportReason === "inappropriate" && "active"
+                      }`}
+                    >
+                      Inappropriate Content
+                    </button>
+                  </li>
+                </ul>
               </div>
-              <h4 className='textarea-header'>
-                {"Additional comments (optional)"}
-              </h4>
-              <textarea
-                id='additional-comments'
-                name='additional-comments'
-                className='additional-comments-textarea'
-                value={additionalComments}
-                onChange={(e) => {
-                  setAdditionalComments(e.target.value);
-                }}
-                maxLength='2200'
-                placeholder='Write additional comments...'
-                aria-label='Add additional comments. Max length 2200 characters.'
-              ></textarea>
+              <div className='textarea-div'>
+                <h4 className='textarea-header'>
+                  {"Additional comments (optional)"}
+                </h4>
+                <textarea
+                  id='additional-comments'
+                  name='additional-comments'
+                  className='additional-comments-textarea'
+                  value={additionalComments}
+                  onChange={(e) => {
+                    setAdditionalComments(e.target.value);
+                  }}
+                  maxLength='2200'
+                  placeholder='Write additional comments...'
+                  aria-label='Add additional comments. Max length 2200 characters.'
+                ></textarea>
+                <span
+                  className='textarea-count'
+                  aria-label='length of additional comments'
+                >
+                  {additionalComments.length} / 2200
+                </span>
+              </div>
               <button className='submit-button' type='submit'>
                 Submit Report
               </button>
