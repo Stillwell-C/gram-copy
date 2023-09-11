3b931df93787a733a0197201a6f3b3564fb68595
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-11 16:49:52 +0900

    Remove unused code

--- src/components/feedRightInfo/FeedRightInfo.js
+++ src/components/feedRightInfo/FeedRightInfo.js
@@ -1,11 +1,5 @@
-import { signOut } from "firebase/auth";
-import React, { useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/authContext";
-import { auth } from "../../firebase";
-import defaultProfilePic from "../../assets/Default_pfp.svg";
-import useGetLoggedInUserInfo from "../../hooks/useGetLoggedInUserInfo";
-import useGetLoggedInUserInfoFunction from "../../hooks/useGetLoggedInUserInfoFunction";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import Footer from "../footer/Footer";
 import "./feedRightInfo.scss";
 import useAuth from "../../hooks/useAuth";
@@ -13,8 +7,6 @@ import { useSendLogoutMutation } from "../../features/auth/authApiSlice";
 
 const FeedRightInfo = () => {
   const { authenticatedUser, username, fullname, img } = useAuth();
-  const { currentUser } = useContext(AuthContext);
-  const navigate = useNavigate();
 
   const userImgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_150,c_fill/${img}`;
 
@@ -28,44 +20,6 @@ const FeedRightInfo = () => {
   useEffect(() => {
     if (isError) window.location.reload();
   }, [isError]);
-
-  //TODO: combine this with same call in userFeed and send down through state
-  // const { userImgURL, username, fullname } = useGetLoggedInUserInfo();
-  // const getUserInfo = useGetLoggedInUserInfoFunction();
-
-  // const [displayUser, setDisplayUser] = useState(false);
-  // const [pageData, setPageData] = useState({
-  //   username: "",
-  //   userImgURL: defaultProfilePic,
-  //   fullname: "",
-  // });
-
-  // useEffect(() => {
-  //   const setInitialData = () => {
-  //     setDisplayUser(true);
-  //     setPageData({
-  //       username: currentUser.displayName,
-  //       userImgURL: currentUser.photoURL,
-  //       fullname: "",
-  //     });
-  //   };
-  //   const fetchAllPageData = async () => {
-  //     const fetchedUserInfo = await getUserInfo().catch((err) =>
-  //       console.log(err.code)
-  //     );
-  //     setPageData({
-  //       username: fetchedUserInfo.username,
-  //       userImgURL: fetchedUserInfo.userImgURL,
-  //       fullname: fetchedUserInfo.fullname,
-  //     });
-  //   };
-  //   if (!currentUser) {
-  //     setDisplayUser(false);
-  //     return;
-  //   }
-  //   currentUser.displayName && setInitialData();
-  //   currentUser.displayName && fetchAllPageData();
-  // }, [currentUser]);
 
   const handleLogout = () => {
     if (authenticatedUser) {
