1ca59cbbf779a7819852dc159b603d6aec7d7dc6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-12 16:15:38 +0900

    Modify classnames

--- src/components/noUserImgProfileFeed/NoUserImgProfileFeed.js
+++ src/components/noUserImgProfileFeed/NoUserImgProfileFeed.js
@@ -1,32 +1,29 @@
 import React from "react";
 import camera from "../../assets/camera-player-multimedia-svgrepo-com.svg";
-import "./noUserImgProfileFeed.scss";
 
 const NoUserImgProfileFeed = ({ handleAddPostModal }) => {
   return (
-    <div className='feed-empty-container'>
-      <div className='share-photo-info'>
-        <button
-          type='button'
-          className='camera-icon-button'
-          aria-label='upload an image from your computer'
-          onClick={handleAddPostModal}
-        >
-          <img src={camera} alt='camera' />
-        </button>
-        <h3 className='header-text'>Share Photos</h3>
-        <div className='sub-text'>
-          When you share photos, they will appear on your profile
-        </div>
-        <button
-          type='button'
-          className='bottom-upload-button'
-          aria-label='upload an image from your computer'
-          onClick={handleAddPostModal}
-        >
-          Share your first photo
-        </button>
+    <div className='share-photo-info flex-container flex-column flex-align-center flex-justify-center'>
+      <button
+        type='button'
+        className='camera-icon-button transparent-button'
+        aria-label='upload an image from your computer'
+        onClick={handleAddPostModal}
+      >
+        <img src={camera} alt='camera' />
+      </button>
+      <h2 className='header-text'>Share Photos</h2>
+      <div className='sub-text'>
+        When you share photos, they will appear on your profile
       </div>
+      <button
+        type='button'
+        className='bottom-upload-button transparent-button'
+        aria-label='upload an image from your computer'
+        onClick={handleAddPostModal}
+      >
+        Share your first photo
+      </button>
     </div>
   );
 };
