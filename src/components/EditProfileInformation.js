b111ba5e9abdddbcb3e3b831bac8216556aea6b6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-18 18:41:18 +0900

    Update imports

--- src/components/EditProfileInformation.js
+++ src/components/EditProfileInformation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import DeleteAccountModal from "./deleteAccountModal/DeleteAccountModal";
+import DeleteAccountModal from "./DeleteAccountModal";
 import { useDispatch } from "react-redux";
 import EditProfileInformationForm from "./EditProfileInformationForm";
 import EditProfileInformationImage from "./EditProfileInformationImage";
