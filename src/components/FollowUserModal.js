14e329e84161b3d43a908658c9ddb4f2394bb2d2
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-19 11:46:06 +0900

    Update classnames & move file

--- src/components/FollowUserModal.js
+++ src/components/FollowUserModal.js
@@ -14,29 +14,30 @@ const FollowUserModal = ({
   };
 
   const content = (
-    <div className='follow-user-modal-container'>
+    <div className='follow-user-modal-container modal-body'>
       <div
-        className='follow-user-modal-body'
+        className='follow-user-modal-body flex-container flex-column flex-align-center width-100 height-100'
         role='dialog'
         aria-labelledby='dialog-header'
       >
-        <div className='follow-user-modal-header'>
+        <div className='follow-user-modal-header width-100 flex-container flex-align-center flex-justify-center'>
           <div></div>
-          <div className='modal-header-text'>
+          <div className='modal-header-text flex-container flex-align-center flex-justify-center'>
             <h2 id='dialog-header'>
               {modalType.slice(0, 1).toUpperCase() + modalType.slice(1)}
             </h2>
           </div>
-          <div className='close-div'>
+          <div className='close-div flex-container flex-align-center'>
             <button
               onClick={() => setShowModal(false)}
               aria-label='click to close'
+              className='transparent-button'
             >
               &times;
             </button>
           </div>
         </div>
-        <div className='follow-user-modal-content'>
+        <div className='follow-user-modal-content width-100 height-100 flex-container flex-column'>
           {users}
           <div className='loading-div'>
             {(isFetching || isLoading) && (
