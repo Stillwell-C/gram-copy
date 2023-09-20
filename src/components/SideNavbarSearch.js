6b5c5bd5952dfae2b535269f38bead9ecc28f17e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-20 17:40:01 +0900

    Remove unused code

--- src/components/SideNavbarSearch.js
+++ src/components/SideNavbarSearch.js
@@ -1,8 +1,3 @@
-import React from "react";
-import { FadeLoader } from "react-spinners";
-import closeCircle from "../assets/close-circle-svgrepo-com.svg";
-import NavbarSearch from "./NavbarSearch";
-
 const SideNavbarSearch = ({ navbarSearch }) => {
   return (
     <div className='search-body'>
@@ -10,42 +5,8 @@ const SideNavbarSearch = ({ navbarSearch }) => {
         <div className='search-header-div'>
           <h2>Search</h2>
         </div>
-        {/* <div className='search-input-div'>
-          <label
-            aria-label='input username to search users'
-            htmlFor='username-search-input'
-          >
-            <input
-              type='text'
-              autoComplete='off'
-              name='username-search-input'
-              id='username-search-input'
-              placeholder='User Search'
-              spellCheck='false'
-              maxLength='30'
-              value={searchQuery}
-              onChange={(e) => handleSearch(e.target.value)}
-            />
-          </label>
-          <div className='input-btn-div'>
-            <button
-              aria-label='clear the search box'
-              onClick={handleClearSearch}
-            >
-              <img src={closeCircle} alt='' aria-hidden='true' />
-            </button>
-          </div>
-        </div> */}
       </div>
       {navbarSearch}
-      {/* <div className='search-bottom'>
-        {searchQuery.length > 0 && !searchIsLoading && searchResults}
-        {searchIsLoading && searchIsFetching && (
-          <div className='loading-div'>
-            <FadeLoader color='#333' cssOverride={{ scale: "0.5" }} />
-          </div>
-        )}
-      </div> */}
     </div>
   );
 };
