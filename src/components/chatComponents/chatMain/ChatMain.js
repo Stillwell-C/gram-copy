645ff71d5cf184a9675a0300ead479f709d760b0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-20 18:57:26 +0900

    Add under construction message

--- src/components/chatComponents/chatMain/ChatMain.js
+++ src/components/chatComponents/chatMain/ChatMain.js
@@ -29,34 +29,39 @@ const ChatMain = () => {
   }, [userData]);
 
   return (
-    <section className='chat-main-container'>
-      <div className='chat-main-body'>
-        <div className='chat-main-left'>
-          <div className='chat-left-top'>
-            <div></div>
-            <div className='username-display'>{username}</div>
-            <div className='top-button-div'>
-              <button
-                className='create-msg-btn'
-                aria-label='click to write a new message'
-                onClick={() => setShowSendMessageModal(true)}
-              >
-                <img src={sendNewMessage} alt='pencil writing message icon' />
-              </button>
-            </div>
-          </div>
-          <div className='chat-left-bottom'>
-            <ChatList />
-          </div>
-        </div>
-        <div className='chat-main-right'>
-          {displayChat && <ChatContainer />}
-        </div>
-      </div>
-      {showSendMessageModal && (
-        <SendMessageModal setShowSendMessageModal={setShowSendMessageModal} />
-      )}
-    </section>
+    // <main className='chat-main-container'>
+    //   <div className='chat-main-body'>
+    //     <div className='chat-main-left'>
+    //       <div className='chat-left-top'>
+    //         <div></div>
+    //         <div className='username-display'>{username}</div>
+    //         <div className='top-button-div'>
+    //           <button
+    //             className='create-msg-btn'
+    //             aria-label='click to write a new message'
+    //             onClick={() => setShowSendMessageModal(true)}
+    //           >
+    //             <img src={sendNewMessage} alt='pencil writing message icon' />
+    //           </button>
+    //         </div>
+    //       </div>
+    //       <div className='chat-left-bottom'>
+    //         <ChatList />
+    //       </div>
+    //     </div>
+    //     <div className='chat-main-right'>
+    //       {displayChat && <ChatContainer />}
+    //     </div>
+    //   </div>
+    //   {showSendMessageModal && (
+    //     <SendMessageModal setShowSendMessageModal={setShowSendMessageModal} />
+    //   )}
+    // </main>
+    <main className='flex-container flex-align-center flex-justify-center flex-column fg-1'>
+      <h2 className='margin-btm-1'>Page under construction</h2>
+      <p>Sorry for the inconvenience.</p>
+      <p>Please check back later.</p>
+    </main>
   );
 };
 
