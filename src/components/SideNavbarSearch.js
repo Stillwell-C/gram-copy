0bce72ba6bc5f5d73e23fb110c0baa3af3e5f0f6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-20 17:42:55 +0900

    Update classnames

--- src/components/SideNavbarSearch.js
+++ src/components/SideNavbarSearch.js
@@ -1,6 +1,6 @@
 const SideNavbarSearch = ({ navbarSearch }) => {
   return (
-    <div className='search-body'>
+    <div className='search-body flex-container flex-column height-100'>
       <div className='search-top'>
         <div className='search-header-div'>
           <h2>Search</h2>
