440608b80a8da2b95f7c50b05960c0f0d0ad9e6a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-20 16:33:26 +0900

    Adjust classnames & jsx elements

--- src/components/headerBar/HeaderBar.js
+++ src/components/headerBar/HeaderBar.js
@@ -4,10 +4,13 @@ import logo from "../../assets/Instagram_logo.png";
 import squareLogo from "../../assets/instagram-svgrepo-com.svg";
 import { Link } from "react-router-dom";
 
-const HeaderBar = ({ navbarSearch, notificationsLink }) => {
+const HeaderBar = ({ navbarSearch, notificationsLink, setSearchActive }) => {
   return (
-    <div className='header-bar-container'>
-      <div className='header-bar-left'>
+    <header className='header-bar-container fg-1'>
+      <div
+        className='header-bar-left height-100 flex-container flex-align-center'
+        onClick={() => setSearchActive(false)}
+      >
         <Link to='/'>
           <picture>
             <source srcSet={logo} media='(min-width:648px)' />
@@ -21,11 +24,13 @@ const HeaderBar = ({ navbarSearch, notificationsLink }) => {
           </picture>
         </Link>
       </div>
-      <div className='header-bar-right'>
+      <div className='header-bar-right height-100 flex-container flex-align-center'>
         {navbarSearch}
-        <div className='navbar-row'>{notificationsLink}</div>
+        <div className='navbar-row flex-container flex-align-center flex-justify-start'>
+          {notificationsLink}
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
