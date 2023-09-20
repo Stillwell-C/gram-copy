cd750ae4f91436b9c75e124df234c0dee43df04a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-20 18:26:44 +0900

    Update Navbar import location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar";
 import ProfileMain from "./components/ProfileMain";
 import Layout from "./components/Layout";
 import ChatMain from "./components/chatComponents/chatMain/ChatMain";
