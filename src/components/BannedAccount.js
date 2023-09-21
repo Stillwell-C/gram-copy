6905f40fb2a48e56d6e09192415af8e3cf4a2ad6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-21 09:31:43 +0900

    Change wording

--- src/components/BannedAccount.js
+++ src/components/BannedAccount.js
@@ -6,7 +6,7 @@ const BannedAccount = () => {
     <main className='fg-1 banned-page flex-container flex-column flex-align-center gap-1'>
       <img alt='' src={prohibited} />
       <h2>User Banned</h2>
-      <p>This user has been banned for violating community guidelines.</p>
+      <p>This user has been banned for violating community standards.</p>
     </main>
   );
 };
