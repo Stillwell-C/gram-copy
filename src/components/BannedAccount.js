1f55202f58a0177b5a4ec9f564e13b987fc5a489
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-21 09:28:48 +0900

    Initial commit

---
+++ src/components/BannedAccount.js
@@ -0,0 +1,14 @@
+import React from "react";
+import prohibited from "../assets/blocked-svgrepo-com.svg";
+
+const BannedAccount = () => {
+  return (
+    <main className='fg-1 banned-page flex-container flex-column flex-align-center gap-1'>
+      <img alt='' src={prohibited} />
+      <h2>User Banned</h2>
+      <p>This user has been banned for violating community guidelines.</p>
+    </main>
+  );
+};
+
+export default BannedAccount;
