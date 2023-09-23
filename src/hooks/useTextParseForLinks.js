7ad12a9990a5f25fc957e6d91e0d2b3abe17dc62
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-09-23 13:48:48 +0900

    Initial commit

---
+++ src/hooks/useTextParseForLinks.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const useParseTextForLinks = () => {
+  const parseTextForLinks = (textInput, maxLength, abbreviate = false) => {
+    const hashtagRegex = /#(\w+)/g;
+    const atRegex = /@(\w+)/g;
+    const commentLines = textInput.split("\n").slice(0, maxLength);
+    const parsedText = commentLines.map((line) => {
+      const lineWords = line.split(" ");
+      return lineWords.map((fragment, i) => {
+        const key = `${fragment}-${i}`;
+        let parsedFragment = fragment;
+        if (hashtagRegex.test(fragment)) {
+          parsedFragment = (
+            <Link to={`/search/hash/${fragment.substring(1)}`}>{fragment}</Link>
+          );
+        }
+        if (atRegex.test(fragment)) {
+          parsedFragment = (
+            <Link to={`/${fragment.substring(1)}`}>{fragment}</Link>
+          );
+        }
+        const spaceOrBreak = i + 1 === lineWords.length ? <br /> : " ";
+        return (
+          <React.Fragment key={key}>
+            {parsedFragment}
+            {spaceOrBreak}
+          </React.Fragment>
+        );
+      });
+    });
+    return (
+      <span className='comment-body'>
+        {parsedText}
+        {abbreviate && textInput.split("\n").length > maxLength && <>...</>}
+      </span>
+    );
+  };
+
+  return parseTextForLinks;
+};
+
+export default useParseTextForLinks;
