5c6a3ccd37cab0aef254af1bc9fbd1ec7c229943
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 15:20:48 +0900

    Add errorModal

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,15 +4,19 @@ import "../scss/global.scss";
 import { useSelector } from "react-redux";
 import { selectLoadingState } from "../features/display/displaySlice";
 import LoadingFullPage from "./LoadingFullPage";
+import { selectError } from "../features/error/errorSlice";
+import ErrorModal from "./ErrorModal";
 
 const Layout = () => {
   const loadingState = useSelector(selectLoadingState);
+  const error = useSelector(selectError);
 
   return (
     <>
       <LoadingFullPage loadingState={loadingState} />
       <div className={styles.layout}>
         <Outlet />
+        {error && <ErrorModal />}
       </div>
     </>
   );
