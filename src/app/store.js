2c2663de92086b9610789d796cb46005d2a7cd7e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 14:43:15 +0900

    Remove apiSlice code

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice";
 import displayReducer from "../features/display/displaySlice";
 
 export const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     display: displayReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
