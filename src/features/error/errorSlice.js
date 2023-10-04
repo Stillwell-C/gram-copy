dcbae619a13d527dd1f8fc1d2e0d9a07eff9d9d3
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 16:21:26 +0900

    Add errorRefreshPage reducer

--- src/features/error/errorSlice.js
+++ src/features/error/errorSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const errorSlice = createSlice({
   name: "error",
-  initialState: { error: false, errorMessage: "" },
+  initialState: { error: false, errorMessage: "", errorRefreshPage: false },
   reducers: {
     setError: (state, action) => {
       return { ...state, error: action.payload };
@@ -10,13 +10,19 @@ const errorSlice = createSlice({
     setErrorMessage: (state, action) => {
       return { ...state, errorMessage: action.payload };
     },
+    setErrorRefreshPage: (state, action) => {
+      return { ...state, errorRefreshPage: action.payload };
+    },
   },
 });
 
 export default errorSlice.reducer;
 
-export const { setError, setErrorMessage } = errorSlice.actions;
+export const { setError, setErrorMessage, setErrorRefreshPage } =
+  errorSlice.actions;
 
 export const selectError = (state) => state.error.error;
 
 export const selectErrorMessage = (state) => state.error.errorMessage;
+
+export const selectErrorRefreshPage = (state) => state.error.errorRefreshPage;
