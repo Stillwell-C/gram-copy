a630b278672e2d68c5bd1288395e0140ea3038d0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 10:28:23 +0900

    Remove unused context apis

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthContextProvider } from "./context/authContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ChatContextProvider } from "./context/chatContext";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -17,14 +15,10 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <BrowserRouter>
-          <AuthContextProvider>
-            <ChatContextProvider>
-              <Routes>
-                <Route path='/*' element={<App />} />
-              </Routes>
-              <ReactQueryDevtools />
-            </ChatContextProvider>
-          </AuthContextProvider>
+          <Routes>
+            <Route path='/*' element={<App />} />
+          </Routes>
+          <ReactQueryDevtools />
         </BrowserRouter>
       </Provider>
     </QueryClientProvider>
