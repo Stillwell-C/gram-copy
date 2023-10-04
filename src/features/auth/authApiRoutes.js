da922b49799c682c1acebae79bd40ce71dff9ef7
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 11:15:23 +0900

    Add logout post route

--- src/features/auth/authApiRoutes.js
+++ src/features/auth/authApiRoutes.js
@@ -1,6 +1,6 @@
 import gramCopyApi from "../../app/api/gramCopyApi";
 import { store } from "../../app/store";
-import { setCredentials } from "./authSlice";
+import { logOut, setCredentials } from "./authSlice";
 
 export const refresh = async () => {
   const response = await gramCopyApi.get(`/auth/refresh`);
@@ -9,4 +9,17 @@ export const refresh = async () => {
   store.dispatch(setCredentials({ accessToken }));
 
   return response;
+};
+
+export const logout = async () => {
+  try {
+    await gramCopyApi.post("/auth/logout");
+    //Extra assurance that loggedIn cookie is cleared
+    document.cookie = "loggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    store.dispatch(logOut());
+    window.location.reload();
+  } catch (err) {
+    console.log(err);
+    window.location.reload();
+  }
 };
