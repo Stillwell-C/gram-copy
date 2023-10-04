1c11237bf09702b1e1a79a03585e894f86c90f2c
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-04 17:34:13 +0900

    Move navigate to ensure proper working upon error

--- src/features/auth/PersistentLogin.js
+++ src/features/auth/PersistentLogin.js
@@ -1,38 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
-import usePersistentLogin from "../../hooks/usePersistentLogin";
 import { selectCurrentToken } from "./authSlice";
 import { useEffect, useRef, useState } from "react";
-import { useRefreshMutation } from "./authApiSlice";
-import LoadingFullPage from "../../components/LoadingFullPage";
 import { Outlet, useNavigate } from "react-router-dom";
 import { setLoading } from "../display/displaySlice";
 import { logout, refresh } from "./authApiRoutes";
 import { useMutation } from "react-query";
 
 const PersistentLogin = () => {
-  // const [persistentLogin, setPersistentLogin] = usePersistentLogin();
   const accessToken = useSelector(selectCurrentToken);
   const runEffect = useRef(false);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  const [loginError, setLoginError] = useState(false);
   const [loginUninitalized, setLoginUninitialized] = useState(true);
 
   const loggedInCookie = document.cookie.match(/^loggedIn=true$/i);
 
   const logoutMutation = useMutation({
     mutationFn: logout,
-    onSuccess: () => {
-      navigate("/accounts/login", {
-        replace: true,
-        state: {
-          errorMessage: "Please log in again.",
-        },
-      });
-    },
   });
 
   const refreshMutation = useMutation({
@@ -61,10 +47,20 @@ const PersistentLogin = () => {
 
   let pageContent;
 
-  if (!loggedInCookie || loginSuccess || (accessToken && loginUninitalized)) {
+  if (
+    !loggedInCookie ||
+    refreshMutation.isSuccess ||
+    (accessToken && loginUninitalized)
+  ) {
     pageContent = <Outlet />;
   } else if (refreshMutation.isError) {
     pageContent = <Outlet />;
+    navigate("/accounts/login", {
+      replace: true,
+      state: {
+        errorMessage: "Please log in again.",
+      },
+    });
   }
 
   return pageContent;
