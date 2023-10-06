a8fe596fa02134c8a1ba7de47b32225252ebb7ad
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-06 16:24:49 +0900

    Add error dispatch upon error

--- src/hooks/useCloudinaryUpload.js
+++ src/hooks/useCloudinaryUpload.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import gramCopyApi from "../app/api/gramCopyApi";
+import { useDispatch } from "react-redux";
+import { setError, setErrorRefreshPage } from "../features/error/errorSlice";
 
 const api_key = "419818228346469";
 const cloud_name = "danscxcd2";
 
 const useCloudinaryUpload = () => {
+  const dispatch = useDispatch();
+
   const uploadImage = async (imageFile) => {
     const signatureResponse = await gramCopyApi.get("/auth/cloud-signature");
 
@@ -14,22 +18,14 @@ const useCloudinaryUpload = () => {
     imgData.append("signature", signatureResponse.data.signature);
     imgData.append("timestamp", signatureResponse.data.timestamp);
 
-    console.log(imgData);
-
     try {
       const cloudinaryRes = await axios.post(
         `https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`,
         imgData,
         {
           headers: { "Content-Type": "multipart/form-data" },
-          //Remove for production
-          onUploadProgress: function (e) {
-            console.log(e.loaded / e.total);
-          },
         }
       );
-
-      console.log(cloudinaryRes);
 
       return {
         public_id: cloudinaryRes.data.public_id,
@@ -38,7 +34,8 @@ const useCloudinaryUpload = () => {
         format: cloudinaryRes.data.format,
       };
     } catch (err) {
-      console.log(err);
+      dispatch(setError(true));
+      dispatch(setErrorRefreshPage(false));
       return err;
     }
   };
