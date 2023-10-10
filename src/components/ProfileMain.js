d66dd1bcefcf106ab8d2c4c372fe5a6db72caeed
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-10 13:31:18 +0900

    Move top of page to own component

--- src/components/ProfileMain.js
+++ src/components/ProfileMain.js
@@ -9,8 +9,11 @@ import threeDots from "../assets/three-dots-line-svgrepo-com.svg";
 import AdditionalOptionsModal from "./AdditionalOptionsModal";
 import ReportModal from "../features/reports/ReportModal";
 import useAuth from "../hooks/useAuth";
-import { useDispatch } from "react-redux";
-import { setLoading } from "../features/display/displaySlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectLoadingState,
+  setLoading,
+} from "../features/display/displaySlice";
 import ProfilePosts from "../features/posts/ProfilePosts";
 import ProfileSaved from "../features/saved/ProfileSaved";
 import ProfileTagged from "../features/posts/ProfileTagged";
@@ -24,10 +27,12 @@ import ProfileUserImage from "../features/users/ProfileUserImage";
 import useParseNumber from "../hooks/useParseNumber";
 import BannedAccount from "./BannedAccount";
 import useParseTextForLinks from "../hooks/useTextParseForLinks";
+import ProfileTop from "./ProfileTop";
 
 const ProfileMain = () => {
   const { userID } = useParams();
   const { authenticatedUser, username } = useAuth();
+
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -90,148 +95,20 @@ const ProfileMain = () => {
 
   const displayOwnPage = authenticatedUser && username === userID;
 
-  const handleMessage = () => {
-    if (!authenticatedUser) {
-      navigate("/accounts/login");
-      return;
-    }
-    navigate("/direct/inbox");
-  };
-
-  const friendButton = userData?.isFollow ? (
-    <UnfollowButton
-      user={userData}
-      queryKey={{
-        key: ["userInfo", userData?.username],
-        multipleInvalidation: false,
-      }}
-    />
-  ) : (
-    <FollowButton
-      user={userData}
-      queryKey={{
-        key: ["userInfo", userData?.username],
-        multipleInvalidation: false,
-      }}
+  const profileTop = (
+    <ProfileTop
+      displayOwnPage={displayOwnPage}
+      userData={userData}
+      setShowAdditionalOptionsModal={setShowAdditionalOptionsModal}
+      setShowFollowerModal={setShowFollowerModal}
+      setShowFollowingModal={setShowFollowingModal}
     />
   );
-
-  const ownPageButtons = (
-    <div className='user-info-buttons flex-container flex-align-center flex-justify-start'>
-      <div className='edit-profile'>
-        <Link to='/accounts/edit'>
-          <button
-            className='edit-profile-btn standard-button grey-button'
-            aria-label='edit profile information'
-          >
-            Edit profile
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-
-  const optionsButton = (
-    <div className='options-div'>
-      <button
-        className='options-button transparent-button'
-        aria-label='click for more options'
-        onClick={() => setShowAdditionalOptionsModal(true)}
-      >
-        <img
-          src={threeDots}
-          className='themeable-icon'
-          alt=''
-          aria-hidden='true'
-        />
-      </button>
-    </div>
-  );
-
-  const defaultPageButtons = (
-    <div className='user-info-buttons flex-container flex-align-center flex-justify-start'>
-      <div className='follow-div'>{friendButton}</div>
-      <div className='message-div'>
-        <button
-          className='message-button standard-button grey-button'
-          aria-label='click to message user'
-          type='button'
-          onClick={handleMessage}
-        >
-          Message
-        </button>
-      </div>
-      {optionsButton}
-    </div>
-  );
-
-  const userInfoButtons =
-    username === userID ? ownPageButtons : defaultPageButtons;
-
-  const userStats = (
-    <>
-      <div>
-        <span className='user-figure'>{parseNum(userData?.postNo)}</span>
-        <span className='category'>
-          {userData?.posts === 1 ? "post" : "posts"}
-        </span>
-      </div>
-      <div
-        className='clickable'
-        aria-label='click to see followers dialog'
-        onClick={() => setShowFollowerModal(true)}
-      >
-        <span className='user-figure'>{parseNum(userData?.followerNo)}</span>
-        {userData?.followerNo === 1 ? "follower" : "followers"}
-        <span className='category'></span>
-      </div>
-      <div
-        className='clickable'
-        aria-label='click to see following dialog'
-        onClick={() => setShowFollowingModal(true)}
-      >
-        <span className='user-figure'>{parseNum(userData?.followingNo)}</span>
-        following<span className='category'></span>
-      </div>
-    </>
-  );
-
-  const parsedUserBio =
-    userData?.userBio.length > 0 &&
-    parseTextForLinks(userData?.userBio, 5, false);
 
   const profilePage = (
     <main className='profile-main-container fg-1 flex-container flex-column flex-align-center'>
       <div className='profile-content-container width-100 flex-container flex-column'>
-        <div className='profile-top flex-container'>
-          <ProfileUserImage displayOwnPage={displayOwnPage} user={userData} />
-          <div className='user-info'>
-            <div className='user-info-top flex-container flex-column flex-align-start flex-justify-center'>
-              <div className='user-info-top-heading flex-container flex-align-center'>
-                <div className='user-info-username'>{userID}</div>
-                {!authenticatedUser && optionsButton}
-              </div>
-              {userInfoButtons}
-            </div>
-            <div className='user-info-middle flex-align-center flex-justify-start'>
-              {userStats}
-            </div>
-            <div className='user-info-bottom'>
-              <div className='user-fullname'>{userData?.fullname}</div>
-              <div className='user-bio'>{parsedUserBio}</div>
-              {userData?.urlLink.length > 0 && (
-                <div className='user-link'>
-                  <a href={userData?.urlLink}>
-                    {userData?.urlLinkText || userData?.urlLink}
-                  </a>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className='user-info-stats-small width-100 flex-container flex-align-center'>
-          {userStats}
-        </div>
+        {profileTop}
         <div className='profile-bottom width-100'>
           <nav
             className='display-selector'
@@ -321,9 +198,9 @@ const ProfileMain = () => {
 
   const pageContent = !userData?.banned ? profilePage : bannedUserPage;
 
-  const returnedContent = !isLoading ? pageContent : null;
+  const returnContent = isLoading ? null : pageContent;
 
-  return returnedContent;
+  return returnContent;
 };
 
 export default ProfileMain;
