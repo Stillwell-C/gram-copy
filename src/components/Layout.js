7561079b12cfee04920a5e76a82a5740a2e61be8
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-11 16:43:11 +0900

    Remove useAuth

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,13 +13,11 @@ import ErrorModal from "./ErrorModal";
 
 import "../scss/layout.scss";
 import "../scss/global.scss";
-import useAuth from "../hooks/useAuth";
 
 const Layout = () => {
   const loadingState = useSelector(selectLoadingState);
   const error = useSelector(selectError);
   const themeState = useSelector(selectThemeState);
-  const { authenticatedUser } = useAuth();
 
   const [theme, setTheme] = useState("theme-light");
 
@@ -39,10 +37,6 @@ const Layout = () => {
     if (!themeState) return;
     setTheme(themeState);
   }, [themeState]);
-
-  useEffect(() => {
-    console.log("from layout ", authenticatedUser);
-  }, [authenticatedUser]);
 
   return (
     <div className={theme}>
