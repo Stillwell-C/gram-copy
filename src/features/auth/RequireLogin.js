f731670c88c99e853c2579db7593a7c5ac96d6df
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-11 16:25:12 +0900

    Return content when loading has completed

--- src/features/auth/RequireLogin.js
+++ src/features/auth/RequireLogin.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 import useAuth from "../../hooks/useAuth";
+import { useSelector } from "react-redux";
+import { selectLoadingState } from "../display/displaySlice";
 
 const RequireLogin = () => {
   const { authenticatedUser } = useAuth();
   const location = useLocation();
+  const loading = useSelector(selectLoadingState);
 
-  const content = authenticatedUser ? (
-    <Outlet />
-  ) : (
-    //This will replace inaccessible page from history
-    <Navigate to='/accounts/login' state={{ from: location }} replace />
-  );
+  let content;
+  if (!loading) {
+    content = authenticatedUser ? (
+      <Outlet />
+    ) : (
+      //This will replace inaccessible page from history
+      <Navigate to='/accounts/login' state={{ from: location }} replace />
+    );
+  }
 
   return content;
 };
