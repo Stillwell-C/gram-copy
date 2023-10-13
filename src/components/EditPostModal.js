2b7080f1e751d2c326792c074714f16a3c72638e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-13 16:18:01 +0900

    Pass setShowPhotoModal to child

--- src/components/EditPostModal.js
+++ src/components/EditPostModal.js
@@ -3,13 +3,21 @@ import FocusTrapModalParent from "./FocusTrapModalParent";
 import "../scss/postModal.scss";
 import EditPostInformationForm from "../features/posts/EditPostInformationForm";
 
-const EditPostModal = ({ setShowEditPostModal, post }) => {
+const EditPostModal = ({
+  setShowEditPostModal,
+  post,
+  setShowPhotoModal = false,
+}) => {
   const handleClose = () => {
     setShowEditPostModal(false);
   };
 
   const content = (
-    <EditPostInformationForm post={post} handleClose={handleClose} />
+    <EditPostInformationForm
+      post={post}
+      handleClose={handleClose}
+      setShowPhotoModal={setShowPhotoModal}
+    />
   );
 
   return <FocusTrapModalParent content={content} handleClose={handleClose} />;
