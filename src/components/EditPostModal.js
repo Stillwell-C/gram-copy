9536ac24635f8a2be8788f69c71e58e30276e4ca
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-13 17:35:58 +0900

    Return as single page

--- src/components/EditPostModal.js
+++ src/components/EditPostModal.js
@@ -2,25 +2,67 @@ import FocusTrapModalParent from "./FocusTrapModalParent";
 
 import "../scss/postModal.scss";
 import EditPostInformationForm from "../features/posts/EditPostInformationForm";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useQuery } from "react-query";
+import { getPost } from "../features/posts/postApiRoutes";
+import { useEffect } from "react";
+import { setLoading } from "../features/display/displaySlice";
 
-const EditPostModal = ({
-  setShowEditPostModal,
-  post,
-  setShowPhotoModal = false,
-}) => {
-  const handleClose = () => {
-    setShowEditPostModal(false);
-  };
+const EditPostModal = () => {
+  const { postID } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const content = (
-    <EditPostInformationForm
-      post={post}
-      handleClose={handleClose}
-      setShowPhotoModal={setShowPhotoModal}
-    />
+  const queryKey = ["singlePost", postID];
+
+  const {
+    data: postData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: queryKey,
+    enabled: !!postID,
+    queryFn: () => getPost(postID),
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (isLoading) {
+      dispatch(setLoading(true));
+      return;
+    }
+    dispatch(setLoading(false));
+  }, [isLoading, postID]);
+
+  useEffect(() => {
+    if (isError && error?.response?.status === 400) {
+      navigate("/error", {
+        replace: true,
+        state: {
+          errorTitle: "Page not found.",
+          errorMessage:
+            "The link you followed may be broken, or the page may have been removed.",
+          errorCode: error?.status,
+        },
+      });
+    }
+    if (isError) {
+      navigate("/error", {
+        replace: true,
+        state: {
+          errorCode: error?.response?.status,
+        },
+      });
+    }
+  }, [isError]);
+
+  return (
+    <div className='flex-container fg-1 flex-align-center flex-justify-center'>
+      {postData?._id && <EditPostInformationForm post={postData} />}
+    </div>
   );
-
-  return <FocusTrapModalParent content={content} handleClose={handleClose} />;
 };
 
 export default EditPostModal;
