a6fd4a607be759871f79f2b34b8aee9fa51a1dca
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-13 17:44:54 +0900

    Update imports

--- src/features/posts/EditPostPage.js
+++ src/features/posts/EditPostPage.js
@@ -6,7 +6,7 @@ import { getPost } from "./postApiRoutes";
 import { useEffect } from "react";
 import { setLoading } from "../display/displaySlice";
 
-import "../scss/postModal.scss";
+import "../../scss/postModal.scss";
 
 const EditPostPage = () => {
   const { postID } = useParams();
