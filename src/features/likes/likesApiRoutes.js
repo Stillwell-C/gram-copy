ec9178f92e9472d625b8c9c50d1f00d2f144a8d6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-18 15:24:30 +0900

    Add getLike route

--- src/features/likes/likesApiRoutes.js
+++ src/features/likes/likesApiRoutes.js
@@ -1,5 +1,13 @@
 import gramCopyApi from "../../app/api/gramCopyApi";
 
+export const getLike = async ({ parentPostID }) => {
+  const response = await gramCopyApi.request({
+    url: `/postlike/post/${parentPostID}`,
+    method: "GET",
+  });
+  return response.data;
+};
+
 export const addNewLike = async ({ parentPostID }) => {
   const response = await gramCopyApi.request({
     url: `/postlike/${parentPostID}`,
