4e1146d748467f9df5a1e71ebbcb8792fc3a3736
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-29 13:25:07 +0900

    Move login buttons to separate component

--- src/components/FeedRightInfo.js
+++ src/components/FeedRightInfo.js
@@ -6,42 +6,24 @@ import LogoutButton from "../features/auth/LogoutButton";
 import useAuth from "../hooks/useAuth";
 
 const FeedRightInfo = () => {
-  const { authenticatedUser, username, fullname, img } = useAuth();
+  const { username, fullname, img } = useAuth();
 
   const userImgURL = `https://res.cloudinary.com/danscxcd2/image/upload/w_150,c_fill/${img}`;
 
   return (
-    <div>
-      {authenticatedUser && (
-        <div className='top-user-info flex-container'>
-          <Link to={`/${username}`}>
-            <img
-              className='circular-image'
-              src={userImgURL}
-              alt='user profile'
-            />
-          </Link>
-          <div className='user-name-div fg-1 flex-container flex-column flex-justify-center flex-align-start'>
-            <Link to={`/${username}`}>
-              <div className='user-name-top'>{username}</div>
-            </Link>
-            <div className='user-name-bottom'>{fullname}</div>
-          </div>
-          <div className='button-div flex-container flex-align-center flex-justify-center'>
-            <LogoutButton />
-          </div>
-        </div>
-      )}
-      {!authenticatedUser && (
-        <div className='login-div'>
-          <Link to='/accounts/login' aria-label='click to log in'>
-            <button className='grey-button'>Log In</button>
-          </Link>
-          <Link to='/accounts/emailsignup' aria-label='click to sign up'>
-            <button className='grey-button'>Sign Up</button>
-          </Link>
-        </div>
-      )}
+    <div className='top-user-info flex-container'>
+      <Link to={`/${username}`}>
+        <img className='circular-image' src={userImgURL} alt='user profile' />
+      </Link>
+      <div className='user-name-div fg-1 flex-container flex-column flex-justify-center flex-align-start'>
+        <Link to={`/${username}`}>
+          <div className='user-name-top'>{username}</div>
+        </Link>
+        <div className='user-name-bottom'>{fullname}</div>
+      </div>
+      <div className='button-div flex-container flex-align-center flex-justify-center'>
+        <LogoutButton />
+      </div>
     </div>
   );
 };
