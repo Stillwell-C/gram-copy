e916b70d4b320c94fddd8b1ed434cc41d8d0aaf1
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-29 13:23:57 +0900

    Add FeedRightLogin

--- src/components/FeedRight.js
+++ src/components/FeedRight.js
@@ -2,11 +2,15 @@ import React from "react";
 import FeedRightInfo from "./FeedRightInfo";
 import Footer from "./Footer";
 import PopularUsers from "../features/users/PopularUsers";
+import useAuth from "../hooks/useAuth";
+import FeedRightLogin from "./FeedRightLogin";
 
 const FeedRight = () => {
+  const { authenticatedUser } = useAuth();
+
   return (
     <section className='right-info-container'>
-      <FeedRightInfo />
+      {authenticatedUser ? <FeedRightInfo /> : <FeedRightLogin />}
       <Footer />
       <div>
         <PopularUsers />
