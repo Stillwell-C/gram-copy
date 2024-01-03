c5484a104bfbc088c23b67afc0936087f6a5e236
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-01-03 15:51:28 +0900

    Initial commit

---
+++ src/features/posts/ProfileUserStatsPosts.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { countPosts } from "./postApiRoutes";
+import useParseNumber from "../../hooks/useParseNumber";
+
+const ProfileUserStatsPosts = ({ userData }) => {
+  const parseNum = useParseNumber();
+
+  const [userPosts, setUserPosts] = useState(userData?.postNo);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["userPostCount", userData._id],
+    queryFn: () => countPosts({ userID: userData._id }),
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setUserPosts(data.postCount);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <span className='user-figure'>{parseNum(userPosts)}</span>
+      <span className='category'>{userPosts === 1 ? "post" : "posts"}</span>
+    </>
+  );
+};
+
+export default ProfileUserStatsPosts;
